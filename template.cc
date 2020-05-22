df58f4cdbf3ac1508f6a63ad14205796d0b62d34
Keisuke Kishimoto <keisuke@google.com>
2020-05-22 21:18:33 +0200

    add ALL, DBG and FOR

--- template.cc
+++ template.cc
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 
+#define ALL(x) (x).begin(), (x).end()
+#define DBG(x) cerr << #x << ": " << (x) << " (L" << __LINE__ << ")" << endl;
+#define FOR(i, n) for (auto i : (n))
 #define REP(i, n) for (int i = 0; i < (int)(n); ++i)
 
 using namespace std;
