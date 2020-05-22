8279e2e61211d5c4c1dc4ecacd88134a0f1cce1c
Keisuke Kishimoto <keisuke@google.com>
2020-05-23 01:36:48 +0200

    DBG supports multiple arguments

--- template.cc
+++ template.cc
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 
+template <typename T>
+void debug(T value) {
+  std::cerr << value;
+}
+
+template <typename T, typename... Ts>
+void debug(T value, Ts... args) {
+  std::cerr << value << ", ";
+  debug(args...);
+}
+
 #define ALL(x) (x).begin(), (x).end()
-#define DBG(x) cerr << #x << ": " << (x) << " (L" << __LINE__ << ")" << endl;
+#define DBG(...)                \
+  cerr << #__VA_ARGS__ << ": "; \
+  debug(__VA_ARGS__);           \
+  cerr << " (L" << __LINE__ << ")" << endl
 #define FOR(i, n) for (auto i : (n))
 #define REP(i, n) for (int i = 0; i < (int)(n); ++i)
 
