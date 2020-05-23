d4eb8083666fd9485c1288f20a313480d89e30cd
Keisuke Kishimoto <keisuke@google.com>
2020-05-23 02:08:33 +0200

    do not execute debug print in the server

--- template.cc
+++ template.cc
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 
+// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
+#if DEBUG
 template <typename T>
 void debug(T value) {
   std::cerr << value;
 }
-
 template <typename T, typename... Ts>
 void debug(T value, Ts... args) {
   std::cerr << value << ", ";
   debug(args...);
 }
-
-#define ALL(x) (x).begin(), (x).end()
 #define DBG(...)                \
   cerr << #__VA_ARGS__ << ": "; \
   debug(__VA_ARGS__);           \
   cerr << " (L" << __LINE__ << ")" << endl
+#else
+#define DBG(...)
+#endif
+
+#define ALL(x) (x).begin(), (x).end()
 #define FOR(i, n) for (auto i : (n))
 #define REP(i, n) for (int i = 0; i < (int)(n); ++i)
 
