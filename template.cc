333be33f405973f0c7d8f521533b28c077a55354
Keisuke Kishimoto <keisuke@google.com>
2020-05-31 02:48:30 +0200

    CIN and COUT

--- template.cc
+++ template.cc
@@ -21,6 +21,27 @@ void debug(T value, Ts... args) {
 #define DBG(...)
 #endif
 
+void read_from_cin() {}
+template <typename T, typename... Ts>
+void read_from_cin(T& value, Ts&... args) {
+  std::cin >> value;
+  read_from_cin(args...);
+}
+#define CIN(type, ...) \
+  type __VA_ARGS__;    \
+  read_from_cin(__VA_ARGS__);
+
+template <typename T>
+void write_to_cout(const T& value) {
+  std::cout << value << std::endl;
+}
+template <typename T, typename... Ts>
+void write_to_cout(const T& value, const Ts&... args) {
+  std::cout << value << ' ';
+  write_to_cout(args...);
+}
+#define COUT(...) write_to_cout(__VA_ARGS__);
+
 #define ALL(x) (x).begin(), (x).end()
 #define FOR(i, n) for (auto i : (n))
 #define REP(i, n) for (int i = 0; i < (int)(n); ++i)
