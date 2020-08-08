dc11de25d7ea1d7a651a0c58abd1e185c024275f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-08 23:25:34 +0200

    use more libraries in abc168

--- contests/abc168/abc168_e/main.cc
+++ contests/abc168/abc168_e/main.cc
@@ -1,67 +1,25 @@
 #include <bits/stdc++.h>
 
 #include "macros.h"
+#include "modint.h"
+#include "rational.h"
 
 using namespace std;
 
-struct Rat {
-  ll sign, n, d;
-  Rat(ll _n, ll _d) {
-    if (_n == 0 || _d == 0) {
-      sign = 0;
-      n = 0;
-      d = 0;
-      return;
-    }
-    sign = 1;
-    if (_n < 0) {
-      sign = -sign;
-      _n = -_n;
-    }
-    if (_d < 0) {
-      sign = -sign;
-      _d = -_d;
-    }
-    ll g = gcd(_n, _d);
-    n = _n / g;
-    d = _d / g;
+using rat = Rational<ll>;
+
+bool operator<(const rat& a, const rat& b) {
+  if (a.p() != b.p()) {
+    return a.p() < b.p();
   }
-  bool operator<(const Rat& r) const {
-    if (sign != r.sign) {
-      return sign < r.sign;
-    }
-    if (n != r.n) {
-      return n < r.n;
-    }
-    return d < r.d;
-  }
-};
-
-map<Rat, int> s1;
-map<Rat, int> s2;
-
-Rat rev(const Rat& r) { return Rat(-r.d, r.n); }
-
-bool eq(const Rat& a, const Rat& b) {
-  return a.sign == b.sign && a.n == b.n && a.d == b.d;
+  return a.q() < b.q();
 }
 
-ll two[200001];
-
-void disp(const Rat& r) {
-  cout << (r.sign < 0 ? "-" : "+") << r.n << "/" << r.d << endl;
-}
+map<rat, int> s1;
+map<rat, int> s2;
 
 int main() {
-  const int kMod = 1000000007;
-
-  two[0] = 1;
-  for (int i = 1; i <= 200000; ++i) {
-    two[i] = two[i - 1] * 2 % kMod;
-  }
-
-  int N;
-  cin >> N;
+  rd(int, N);
 
   ll zeros = 0;
 
@@ -69,8 +27,7 @@ int main() {
   ll B_zeros = 0;
 
   rep(i, N) {
-    ll A, B;
-    cin >> A >> B;
+    rd(ll, A, B);
 
     if (A == 0 && B == 0) {
       ++zeros;
@@ -85,52 +42,27 @@ int main() {
       continue;
     }
 
-    // cout << "A: " << A << " " << "B:"<< B << endl;
-
-    // -B/A
-    Rat r1(-B, A);
-    // disp(r1);
-    s1[r1]++;
-
-    // A/B
-    Rat r2(A, B);
-    // disp(r2);
-    s2[r2]++;
+    s1[rat(-B, A)]++;
+    s2[rat(A, B)]++;
   }
 
   vector<pair<int, int>> groups;
-  for (auto it : s2) {
-    /*
-    if (eq(it.first, rev(it.first))) {
-      pair<int, int> p(it.second, 0);
-      groups.push_back(make_pair(it.second, s1[it.first]));
-      continue;
-    }
-    */
-
-    pair<int, int> p(it.second, s1[it.first]);
-    if (p.second != 0 && it.first.sign < 0) continue;
-
-    groups.push_back(p);
-    // disp(it.first);
-    // cout << "group: " << groups.back().first << " ";
-    // cout << groups.back().second << endl;
+  for (auto [r, c2] : s2) {
+    int c1 = s1[r];
+    if (c1 != 0 && r.p() < 0) continue;
+    groups.push_back({c2, c1});
   }
-  groups.push_back(make_pair(A_zeros, B_zeros));
+  groups.push_back({A_zeros, B_zeros});
 
-  long long mul = 1;
-  for (auto p : groups) {
-    // cout << p.first << " " << p.second << endl;
-    ll m;
-    if (p.second == 0) {
-      m = two[p.first];
+  mint mul = 1;
+  for (auto [a, b] : groups) {
+    mint m;
+    if (b == 0) {
+      m = mint(2).Pow(a);
     } else {
-      m = (two[p.first] + two[p.second] - 1) % kMod;
+      m = mint(2).Pow(a) + mint(2).Pow(b) - 1;
     }
-    // cout << m << endl;
     mul *= m;
-    mul %= kMod;
   }
-  ll ans = (mul - 1 + zeros) % kMod;
-  cout << ans << endl;
+  wt(mul - 1 + zeros);
 }
