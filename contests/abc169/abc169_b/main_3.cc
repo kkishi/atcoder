02f37d0bf8d29f327f83f320ce7d05c2927321a6
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-08 22:23:22 +0200

    update abc169

--- contests/abc169/abc169_b/main_3.cc
+++ contests/abc169/abc169_b/main_3.cc
@@ -1,54 +1,7 @@
 #include <bits/stdc++.h>
-#include <boost/multiprecision/cpp_int.hpp>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define DBG(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define DBG(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define CIN(type, ...) \
-  type __VA_ARGS__;    \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define COUT(...) write_to_cout(__VA_ARGS__);
-
-#define ALL(x) (x).begin(), (x).end()
-#define FOR(i, n) for (auto i : (n))
-#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
-using bigint = boost::multiprecision::cpp_int;
+#include "bignum.h"
+#include "macros.h"
 
 using namespace std;
 
@@ -58,7 +11,7 @@ ll kMax = 1000000000000000000;
 
 int main() {
   cin >> N;
-  REP(i, N) {
+  rep(i, N) {
     cin >> A[i];
     if (A[i] == 0) {
       cout << 0 << endl;
@@ -66,12 +19,12 @@ int main() {
     }
   }
   bigint ans = 1;
-  REP(i, N) {
+  rep(i, N) {
     ans *= A[i];
     if (ans > kMax) {
       cout << -1 << endl;
       return 0;
     }
   }
-  COUT(ans);
+  wt(ans);
 }
--- contests/abc169/abc169_e/main.cc
+++ contests/abc169/abc169_e/main.cc
@@ -1,52 +1,6 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define DBG(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define DBG(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define CIN(type, ...) \
-  type __VA_ARGS__;    \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define COUT(...) write_to_cout(__VA_ARGS__);
-
-#define ALL(x) (x).begin(), (x).end()
-#define FOR(i, n) for (auto i : (n))
-#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
@@ -54,12 +8,12 @@ int A[200000];
 int B[200000];
 
 int main() {
-  CIN(int, N);
-  REP(i, N) cin >> A[i] >> B[i];
+  rd(int, N);
+  rep(i, N) cin >> A[i] >> B[i];
   sort(A, A + N);
   sort(B, B + N);
   if (N % 2 == 0) {
   } else {
-    COUT(B[N / 2] - A[N / 2] + 1);
+    wt(B[N / 2] - A[N / 2] + 1);
   }
 }
