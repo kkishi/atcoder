61c03c7a2b3805ae793c416b1237cd027bf3e8b5
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-08 21:55:49 +0200

    update abc171

--- contests/abc171/abc171_d/main.cc
+++ contests/abc171/abc171_d/main.cc
@@ -1,72 +1,27 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
 int main() {
-  in(int, n);
+  rd(int, n);
   vector<ll> a(n);
   rep(i, n) cin >> a[i];
   map<ll, ll> m;
   rep(i, n) m[a[i]]++;
-  in(int, q);
+  rd(int, q);
   ll sum = accumulate(all(a), 0LL);
   while (q--) {
-    in(ll, b, c);
+    rd(ll, b, c);
     auto it = m.find(b);
     if (it == m.end()) {
-      out(sum);
+      wt(sum);
       continue;
     }
     ll diff = (c - it->first) * it->second;
     sum += diff;
-    out(sum);
+    wt(sum);
     m[c] += it->second;
     m.erase(it);
   }
@@ -75,6 +30,6 @@ int main() {
   for (auto [k, v] : m) {
     ans += k * v;
   }
-  out(ans);
+  wt(ans);
   */
 }
