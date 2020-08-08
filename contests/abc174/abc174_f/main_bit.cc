992181e6ae5b2d43531fbafdc2fc5d039b56fcda
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-08 17:19:20 +0200

    update abc174

--- contests/abc174/abc174_f/main_bit.cc
+++ contests/abc174/abc174_f/main_bit.cc
@@ -1,94 +1,21 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
-
-template <typename T>
-using V = std::vector<T>;
-template <typename T>
-using VV = V<V<T>>;
+#include "bit.h"
+#include "macros.h"
 
 using namespace std;
-
-template <typename T>
-class BIT {
- public:
-  BIT(int n) : v_(n) {}
-  T Sum(int i) const {
-    T ret = 0;
-    while (i >= 0) {
-      ret += v_[i];
-      i = (i & (i + 1)) - 1;
-    }
-    return ret;
-  }
-  T Get(int i) const { return Sum(i) - Sum(i - 1); }
-  void Add(int i, T v) {
-    while (i < v_.size()) {
-      v_[i] += v;
-      i |= i + 1;
-    }
-  }
-
- private:
-  std::vector<T> v_;
-};
 
 struct Q {
   int l, r, i, v;
 };
 
 int main() {
-  in(int, n, q);
+  rd(int, n, q);
   V<int> c(n);
   rep(i, n) cin >> c[i];
   V<Q> qs;
   rep(i, q) {
-    in(int, l, r);
+    rd(int, l, r);
     --l, --r;
     qs.push_back({l, r, i, 0});
   }
@@ -112,5 +39,5 @@ int main() {
       ++qit;
     }
   }
-  rep(i, q) out(ans[i]);
+  rep(i, q) wt(ans[i]);
 }
