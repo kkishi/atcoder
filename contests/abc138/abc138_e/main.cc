471fc5a0dd1a54ef79025320c983136d83242429
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 21:34:28 +0200

    update abc138

--- contests/abc138/abc138_e/main.cc
+++ contests/abc138/abc138_e/main.cc
@@ -1,56 +1,11 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
 int main() {
-  in(string, s, t);
+  rd(string, s, t);
   s = s + s;
   vector<ll> last(26, -1);
   vector<vector<ll>> next(s.size(), vector(26, 0LL));
@@ -67,12 +22,12 @@ int main() {
     ll n = next[si][t[i] - 'a'];
     // dbg(t[i], n);
     if (n == -1) {
-      out(-1);
+      wt(-1);
       return 0;
     }
     ans += n + 1;
     si = (si + n + 1) % (s.size() / 2);
     // dbg(si);
   }
-  out(ans);
+  wt(ans);
 }
