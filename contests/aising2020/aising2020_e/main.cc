ff3f92d3fc7eef93198aa62525cc780a61ad5c6f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 23:18:35 +0200

    update aising2020

--- contests/aising2020/aising2020_e/main.cc
+++ contests/aising2020/aising2020_e/main.cc
@@ -1,51 +1,6 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
@@ -55,11 +10,11 @@ struct Camel {
 };
 
 void solve() {
-  in(int, n);
+  rd(int, n);
   ll ans = 0;
   vector<Camel> lc, rc;
   rep(i, n) {
-    in(ll, k, l, r);
+    rd(ll, k, l, r);
     if (l >= r) {
       ans += r;
       lc.push_back({k, l - r});
@@ -95,10 +50,10 @@ void solve() {
   sort(all(rc), less);
   calc(rc);
 
-  out(ans);
+  wt(ans);
 }
 
 int main() {
-  in(int, t);
+  rd(int, t);
   while (t--) solve();
 }
