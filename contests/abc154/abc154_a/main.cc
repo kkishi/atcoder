a6210a73dd32b69c9826718951e75f1433604f85
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 12:50:27 +0200

    update abc154

--- contests/abc154/abc154_a/main.cc
+++ contests/abc154/abc154_a/main.cc
@@ -1,59 +1,13 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define DBG(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define DBG(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define CIN(type, ...) \
-  type __VA_ARGS__;    \
-  read_from_cin(__VA_ARGS__)
-
-template <typename T>
-void write_to_cout(T&& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(T&& value, Ts&&... args) {
-  std::cout << value << ' ';
-  write_to_cout(std::forward<Ts>(args)...);
-}
-#define COUT(...) write_to_cout(__VA_ARGS__);
-
-#define ALL(x) (x).begin(), (x).end()
-#define FOR(i, n) for (auto i : (n))
-#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
 int main() {
-  CIN(string, S, T);
-  CIN(int, A, B);
-  CIN(string, U);
+  rd(string, S, T);
+  rd(int, A, B);
+  rd(string, U);
   (U == S ? A : B)--;
-  COUT(A, B);
+  wt(A, B);
 }
--- contests/abc154/abc154_e/main.cc
+++ contests/abc154/abc154_e/main.cc
@@ -1,52 +1,6 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define DBG(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define DBG(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define CIN(type, ...) \
-  type __VA_ARGS__;    \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define COUT(...) write_to_cout(__VA_ARGS__);
-
-#define ALL(x) (x).begin(), (x).end()
-#define FOR(i, n) for (auto i : (n))
-#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
@@ -54,10 +8,10 @@ int dp_eq[101][5];
 int dp_lo[101][5];
 
 int main() {
-  CIN(string, N);
-  CIN(int, K);
+  rd(string, N);
+  rd(int, K);
   dp_eq[0][0] = 1;
-  REP(i, N.size()) {
+  rep(i, N.size()) {
     for (int z = 0; z <= 3; ++z) {
       int d = N[i] - '0';
       // Equal to d
@@ -71,5 +25,5 @@ int main() {
       dp_lo[i + 1][z + 1] += dp_lo[i][z] * (9 - d);
     }
   }
-  COUT(dp_eq[N.size()][K] + dp_lo[N.size()][K])
+  wt(dp_eq[N.size()][K] + dp_lo[N.size()][K])
 }
