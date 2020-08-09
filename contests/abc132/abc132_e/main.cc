bce860a3b7483661ed7458f90ad794aaf16034bd
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 22:10:09 +0200

    update abc132

--- contests/abc132/abc132_e/main.cc
+++ contests/abc132/abc132_e/main.cc
@@ -1,62 +1,17 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
 int main() {
-  in(int, n, m);
+  rd(int, n, m);
   vector<vector<int>> g(n + 1);
   rep(i, m) {
-    in(int, u, v);
+    rd(int, u, v);
     g[u].push_back(v);
   }
-  in(int, s, t);
+  rd(int, s, t);
   vector<vector<bool>> visited(n + 1, vector<bool>(3));
   visited[s][0] = true;
   queue<tuple<int, int, int>> que;
@@ -65,7 +20,7 @@ int main() {
     auto [node, step, dist] = que.front();
     que.pop();
     if (node == t && step == 0) {
-      out(dist);
+      wt(dist);
       return 0;
     }
     for (int child : g[node]) {
@@ -80,5 +35,5 @@ int main() {
       que.push({child, nstep, ndist});
     }
   }
-  out(-1);
+  wt(-1);
 }
