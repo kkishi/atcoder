c260546ca5aab969fb4ad9c2bea7f8a0bf068093
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 21:19:38 +0200

    update abc143

--- contests/abc143/abc143_e/main_fast.cc
+++ contests/abc143/abc143_e/main_fast.cc
@@ -1,51 +1,6 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
@@ -59,9 +14,7 @@ struct State {
     }
     return remaining > dp.remaining;
   }
-  bool operator>(const State& dp) const {
-    return dp < *this;
-  }
+  bool operator>(const State& dp) const { return dp < *this; }
 };
 
 ll dist[301][301];
@@ -118,9 +71,9 @@ int main() {
     scanf("%lld %lld", &s, &t);
     State& st = seens[s][t];
     if (st.city == 0) {
-      out(-1);
+      wt(-1);
     } else {
-      out(st.refueling);
+      wt(st.refueling);
     }
   }
 }
