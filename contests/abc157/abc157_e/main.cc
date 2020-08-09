667223a6a04b8bf51eca5981b527e77b6316d213
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 12:06:22 +0200

    update abc157

--- contests/abc157/abc157_e/main.cc
+++ contests/abc157/abc157_e/main.cc
@@ -1,70 +1,7 @@
 #include <bits/stdc++.h>
 
-template <typename T, T Op(T, T), T Unit = T()>
-    class SegmentTree {
- public:
-  SegmentTree(int size) {
-    int two = 1;
-    while (two < size) {
-      two <<= 1;
-    }
-    v_.resize(two * 2 - 1, Unit);
-  }
-  void Set(int i, T v) {
-    int index = v_.size() / 2 + i;
-    while (true) {
-      v_[index] = v;
-      if (index == 0) break;
-      v = Op(v, v_[index + (index % 2 == 0 ? -1 : 1)]);
-      index = (index - 1) / 2;
-    }
-  }
-  T Get(int i) const { return Aggregate(i, i + 1); }
-  T Aggregate(int begin, int end) const {
-    return aggregate(begin, end, 0, (v_.size() + 1) / 2, 0);
-  }
-
- private:
-  T aggregate(int begin, int end, int cbegin, int cend, int index) const {
-    if (begin <= cbegin && cend <= end) {
-      return v_[index];
-    }
-    if (cend <= begin || end <= cbegin) {
-      return Unit;
-    }
-    int cmid = (cbegin + cend) / 2;
-    return Op(aggregate(begin, end, cbegin, cmid, index * 2 + 1),
-              aggregate(begin, end, cmid, cend, index * 2 + 2));
-  }
-  std::vector<T> v_;
-};
-
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define DBG(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define DBG(...)
-#endif
-
-#define ALL(x) (x).begin(), (x).end()
-#define FOR(i, n) for (auto i : (n))
-#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
+#include "segment_tree.h"
 
 using namespace std;
 
@@ -73,14 +10,14 @@ int Union(int a, int b) { return a | b; }
 int main() {
   int N;
   cin >> N;
-  SegmentTree<int, Union> tree(N);
+  SegmentTree<int> tree(N, Union);
   string S;
   cin >> S;
-  REP(i, N) tree.Set(i, 1 << (S[i] - 'a'));
-  REP(i, N) DBG(i, tree.Get(i));
+  rep(i, N) tree.Set(i, 1 << (S[i] - 'a'));
+  rep(i, N) dbg(i, tree.Get(i));
   int Q;
   cin >> Q;
-  REP(q, Q) {
+  rep(q, Q) {
     int t;
     cin >> t;
     if (t == 1) {
--- contests/abc157/abc157_f/main.cc
+++ contests/abc157/abc157_f/main.cc
@@ -1,117 +1,37 @@
 #include <bits/stdc++.h>
 
-template <typename T>
-struct Vector {
-  T x, y;
-  T Norm() const { return std::sqrt(x * x + y * y); }
-  Vector& operator+=(const Vector& v) {
-    (*this).x += v.x;
-    (*this).y += v.y;
-    return *this;
-  }
-  Vector operator+(const Vector& v) const { return Vector(*this) += v; }
-  Vector& operator-=(const Vector& v) {
-    (*this).x -= v.x;
-    (*this).y -= v.y;
-    return *this;
-  }
-  Vector operator-(const Vector& v) const { return Vector(*this) -= v; }
-  Vector& operator*=(T t) {
-    (*this).x *= t;
-    (*this).y *= t;
-    return *this;
-  }
-  Vector operator*(T t) const { return Vector(*this) *= t; }
-  Vector Rot90() const { return {-y, x}; }
-};
+#include "geometry.h"
+#include "macros.h"
 
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const Vector<T>& v) {
-  os << "(" << v.x << "," << v.y << ")";
-  return os;
-}
-
-template <typename T>
-struct Circle {
-  using V = Vector<T>;
-  V center;
-  double radius;
-
-  std::vector<V> Intersections(const Circle& c) const {
-    // TODO: Handle cases where there is no intersection and there is only one
-    // intersection.
-    auto sq = [](T x) -> T { return x * x; };
-    V v = c.center - center;
-    T l = v.Norm();
-    if (l >= radius + c.radius ||
-        (l + radius) <= c.radius ||
-        (l + c.radius) <= radius) {
-      return {};
-    }
-    T x = (sq(radius) - sq(c.radius) + sq(l)) / (2 * l);
-    T a = std::sqrt(sq(radius) - sq(x));
-    V perpendicular_foot = v * (x / l);
-    V perpendicular = v.Rot90() * (a / l);
-    return {center + perpendicular_foot + perpendicular,
-            center + perpendicular_foot - perpendicular};
-  }
-};
-
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define DBG(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define DBG(...)
-#endif
-
-#define ALL(x) (x).begin(), (x).end()
-#define FOR(i, n) for (auto i : (n))
-#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
+using namespace std;
 
 using Vec = Vector<double>;
 using Circ = Circle<double>;
-using ll = long long;
-
-using namespace std;
 
 int N, K;
 Vec p[60];
 double c[60];
 
 bool Ok(double t) {
-  DBG(t);
+  dbg(t);
   vector<Vec> vs(p, p + N);
   auto Rad = [t](int i) { return t / c[i]; };
-  REP(i, N) REP(j, i) {
+  rep(i, N) rep(j, i) {
     Circ ci = {p[i], Rad(i)}, cj = {p[j], Rad(j)};
-    DBG(ci.center, ci.radius, cj.center, cj.radius);
-    FOR(v, ci.Intersections(cj)) {
-      DBG(v);
+    dbg(ci.center, ci.radius, cj.center, cj.radius);
+    for (auto& v : ci.Intersections(cj)) {
+      dbg(v);
       vs.push_back(v);
     }
   }
-  // DBG(t, vs.size());
-  REP(i, vs.size()) {
+  // dbg(t, vs.size());
+  rep(i, vs.size()) {
     int contains = 0;
-    REP(j, N) {
+    rep(j, N) {
       // This 1e-10 was important.
       if (Rad(j) + 1e-10 > (vs[i] - p[j]).Norm()) ++contains;
     }
-    DBG(t, i, vs[i], contains);
+    dbg(t, i, vs[i], contains);
     if (contains >= K) {
       return true;
     }
@@ -121,7 +41,7 @@ bool Ok(double t) {
 
 int main() {
   cin >> N >> K;
-  REP(i, N) cin >> p[i].x >> p[i].y >> c[i];
+  rep(i, N) cin >> p[i].x >> p[i].y >> c[i];
 
   // Initially lo was 0.1 for some reason, which resulted WA.
   double lo = 0, hi = 10000000;
