075e88d11645ee52dd2ff60530f1f215fda3d460
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 22:50:36 +0200

    update abc111

--- contests/abc111/abc111_c/main.cc
+++ contests/abc111/abc111_c/main.cc
@@ -1,56 +1,11 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
 int main() {
-  in(int, n);
+  rd(int, n);
   vector<int> v(n);
   rep(i, n) cin >> v[i];
   vector<vector<pair<int, int>>> counts(2);
@@ -71,12 +26,11 @@ int main() {
   // 2 1 2 1 2 1
 
   if (counts[0][0].second != counts[1][0].second) {
-    out(n - (counts[0][0].first + counts[1][0].first));
+    wt(n - (counts[0][0].first + counts[1][0].first));
   } else {
     vector<int> second(2);
     rep(i, 2) second[i] = counts[i].size() == 1 ? 0 : counts[i][1].first;
     rep(i, 2) dbg(second[i]);
-    out(n -
-        max(counts[0][0].first + second[1], counts[1][0].first + second[0]));
+    wt(n - max(counts[0][0].first + second[1], counts[1][0].first + second[0]));
   }
 }
