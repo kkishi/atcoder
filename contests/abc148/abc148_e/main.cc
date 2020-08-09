83100e2258d7dca0dfd7684354652ac40f773cf3
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 21:00:21 +0200

    update abc148

--- contests/abc148/abc148_e/main.cc
+++ contests/abc148/abc148_e/main.cc
@@ -1,61 +1,14 @@
 #include <bits/stdc++.h>
-#include <boost/multiprecision/cpp_int.hpp>
 
-using bigint = boost::multiprecision::cpp_int;
-
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "bignum.h"
+#include "macros.h"
 
 using namespace std;
 
 int main() {
-  in(ll, n);
+  rd(ll, n);
   if (n % 2) {
-    out(0);
+    wt(0);
   } else {
     ll ans = 0;
     ll fives = 1;
@@ -63,6 +16,6 @@ int main() {
       fives *= 5;
       ans += n / fives / 2;
     }
-    out(ans);
+    wt(ans);
   }
 }
