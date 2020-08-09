586461eaca1d9b39d5132d0358b984deefe7caed
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 23:25:26 +0200

    update tokiomarine2020

--- contests/tokiomarine2020/tokiomarine2020_d/main_2.cc
+++ contests/tokiomarine2020/tokiomarine2020_d/main_2.cc
@@ -1,87 +1,44 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
 
 using namespace std;
 
 const int kMaxCache = 1 << 11;
 const int kMaxL = int(1e5);
-int V[1 << 18];
+int V_[1 << 18];
 int W[1 << 18];
 int dp[kMaxCache][kMaxL + 1];
 
-void Setmax(int& a, int b) { if (a < b) a = b; }
+void Setmax(int& a, int b) {
+  if (a < b) a = b;
+}
 int Parent(int node) { return (node - 1) / 2; }
 
 int main() {
-  in(int, n);
-  rep(i, n) cin >> V[i] >> W[i];
+  rd(int, n);
+  rep(i, n) cin >> V_[i] >> W[i];
 
-  for (int i = W[0]; i <= kMaxL; ++i) dp[0][i] = V[0];
+  for (int i = W[0]; i <= kMaxL; ++i) dp[0][i] = V_[0];
   for (int node = 1; node < min(kMaxCache, n); ++node) {
     int parent = Parent(node);
     for (int i = 0; i <= kMaxL; ++i) dp[node][i] = dp[parent][i];
     for (int i = kMaxL; i - W[node] >= 0; --i) {
-      Setmax(dp[node][i], dp[parent][i - W[node]] + V[node]);
+      Setmax(dp[node][i], dp[parent][i - W[node]] + V_[node]);
     }
   }
 
-  in(int, q);
+  rd(int, q);
   rep(i, q) {
-    in(int, v, l);
+    rd(int, v, l);
     --v;
 
     if (v < kMaxCache) {
-      out(dp[v][l]);
+      wt(dp[v][l]);
     } else {
       vector<int> vs, ws;
       while (v >= kMaxCache) {
-        vs.push_back(V[v]);
+        vs.push_back(V_[v]);
         ws.push_back(W[v]);
         v = Parent(v);
       }
@@ -97,7 +54,7 @@ int main() {
           ans = max(ans, v_sum + dp[v][l - w_sum]);
         }
       }
-      out(ans);
+      wt(ans);
     }
   }
 }
