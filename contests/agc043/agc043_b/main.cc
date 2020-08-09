4729cc59b7f411109c754437c098757444cef2ac
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 23:58:54 +0200

    update agc043

--- contests/agc043/agc043_b/main.cc
+++ contests/agc043/agc043_b/main.cc
@@ -1,36 +1,13 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define DBG(...)                \
-  cerr << #__VA_ARGS__ << ": "; \
-  debug(__VA_ARGS__);           \
-  cerr << " (L" << __LINE__ << ")" << endl
-#else
-#define DBG(...)
-#endif
+#include "macros.h"
+#include "setmax.h"
 
-#define ALL(x) (x).begin(), (x).end()
 #define FOR(i, n) for (auto i : (n))
-#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
-
-template <typename T>
-void setmax(T& a, const T& b) {
-  if (a < b) a = b;
-}
 
 using namespace std;
 
-template<class iter>
+template <class iter>
 string conv(iter begin, iter end) {
   string s;
   for (; begin != end; ++begin) {
@@ -43,9 +20,7 @@ string conv(iter begin, iter end) {
 
 string simulate(const string& s) {
   string ret;
-  REP(i, s.size() - 1) {
-    ret += '0' + abs(s[i] - s[i + 1]);
-  }
+  rep(i, s.size() - 1) { ret += '0' + abs(s[i] - s[i + 1]); }
   return ret;
 }
 
@@ -65,29 +40,29 @@ int main() {
   cin >> N;
   string S;
   cin >> S;
-  string l = conv(ALL(S));
+  string l = conv(all(S));
   string r = conv(S.rbegin(), S.rend());
-  DBG(l, r);
+  dbg(l, r);
   /*
-  reverse(ALL(r));
+  reverse(all(r));
   string s = l.substr(0, l.size() - 1) + r;
-  DBG(l, r, s);
+  dbg(l, r, s);
   */
 
   /*
   while (s.size() > 1) {
     s = simulate(s);
-    DBG(s);
+    dbg(s);
   }
   cout << s << endl;
   */
   char maximum = '0';
-  FOR(c, S) setmax(maximum, c);
-  DBG(maximum);
+  FOR(c, S) Setmax(maximum, c);
+  dbg(maximum);
   int cnt = 0;
-  FOR(c, S) if (c == maximum) ++cnt;
+  FOR(c, S) if (c == maximum)++ cnt;
 
-  reverse(ALL(r));
+  reverse(all(r));
 
   string s;
   if (cnt & 1) {
@@ -95,32 +70,32 @@ int main() {
   } else {
     s = l + r;
   }
-  DBG(s);
+  dbg(s);
 
   while (s.size() > 1) {
     s = simulate(s);
-    DBG(s);
+    dbg(s);
   }
   // cout << s << endl;
 
   string naive = S;
   while (naive.size() > 1) {
     naive = simulate(naive);
-    DBG(naive);
+    dbg(naive);
   }
   if (s != naive) {
-    DBG("FAIL");
+    dbg("FAIL");
   }
 
   string fast = S;
   while (fast.size() > 1) {
     fast = simulate(fast);
-    DBG(fast);
+    dbg(fast);
     fast = simplify(fast);
-    DBG(fast);
+    dbg(fast);
   }
   if (fast != naive) {
-    DBG("FAIL");
+    dbg("FAIL");
   }
   cout << fast << endl;
 }
