029361caaac48f08b300802b7c77c206faa07abf
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 23:16:53 +0200

    update m-solutions2020

--- contests/m-solutions2020/m_solutions2020_e/main.cc
+++ contests/m-solutions2020/m_solutions2020_e/main.cc
@@ -1,65 +1,12 @@
 #include <bits/stdc++.h>
 
-// #undef DEBUG  // Uncomment this line to forcefully disable debug print.
-#if DEBUG
-template <typename T>
-void debug(T value) {
-  std::cerr << value;
-}
-template <typename T, typename... Ts>
-void debug(T value, Ts... args) {
-  std::cerr << value << ", ";
-  debug(args...);
-}
-#define dbg(...)                              \
-  do {                                        \
-    cerr << #__VA_ARGS__ << ": ";             \
-    debug(__VA_ARGS__);                       \
-    cerr << " (L" << __LINE__ << ")" << endl; \
-  } while (0)
-#else
-#define dbg(...)
-#endif
-
-void read_from_cin() {}
-template <typename T, typename... Ts>
-void read_from_cin(T& value, Ts&... args) {
-  std::cin >> value;
-  read_from_cin(args...);
-}
-#define in(type, ...) \
-  type __VA_ARGS__;   \
-  read_from_cin(__VA_ARGS__);
-
-template <typename T>
-void write_to_cout(const T& value) {
-  std::cout << value << std::endl;
-}
-template <typename T, typename... Ts>
-void write_to_cout(const T& value, const Ts&... args) {
-  std::cout << value << ' ';
-  write_to_cout(args...);
-}
-#define out(...) write_to_cout(__VA_ARGS__);
-
-#define all(x) (x).begin(), (x).end()
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-
-using ll = long long;
+#include "macros.h"
+#include "setmin.h"
 
 using namespace std;
 
-template <typename T>
-bool Setmin(T& a, T b) {
-  if (a > b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
-
 int main() {
-  in(ll, n);
+  rd(ll, n);
   vector<ll> x(n), y(n), p(n);
   rep(i, n) cin >> x[i] >> y[i] >> p[i];
   vector<ll> ans(n + 1, numeric_limits<ll>::max());
@@ -89,6 +36,6 @@ int main() {
   rep(i, n) dist[i] = min(abs(x[i]), abs(y[i]));
   rec(0, 0, dist);
   for (int k = 0; k <= n; ++k) {
-    out(ans[k]);
+    wt(ans[k]);
   }
 }
