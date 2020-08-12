14ec5f823c227457803cfb3588bff6751099ccb9
Keisuke Kishimoto <keisuke@google.com>
2020-08-12 23:52:53 +0200

    add solutions for agc046

---
+++ contests/agc046/agc046_a/main.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+#include "macros.h"
+
+using namespace std;
+
+int main() {
+  rd(int, x);
+  wt(lcm(x, 360) / x);
+}
---
+++ contests/agc046/agc046_b/main.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#include "macros.h"
+#include "modint.h"
+
+using mint = ModInt<998244353>;
+
+using namespace std;
+
+int main() {
+  rd(int, a, b, c, d);
+  VV<mint> dp(c + 2, V<mint>(d + 2));
+  dp[a][b] = 1;
+  for (int i = a; i <= c; ++i) {
+    for (int j = b; j <= d; ++j) {
+      dp[i][j] -= dp[i - 1][j - 1] * (i - 1) * (j - 1);
+      dp[i + 1][j] += dp[i][j] * j;
+      dp[i][j + 1] += dp[i][j] * i;
+    }
+  }
+  wt(dp[c][d]);
+}
