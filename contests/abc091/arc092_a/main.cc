e5b0bc59f3d38e55545fe993a3e2cbc3f92befec
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-16 21:26:54 +0200

    rename to edmonds karp

--- contests/abc091/arc092_a/main.cc
+++ contests/abc091/arc092_a/main.cc
@@ -1,42 +1,44 @@
 #include <bits/stdc++.h>
 
 #include "graph.h"
-#include "macros.h"
-
-using namespace std;
 
 template <typename T>
-T FordFulkerson(const Graph<T>& g, int s, int d) {
-  VV<T> flow(g.NumVertices(), V<T>(g.NumVertices()));
+T EdmondsKarp(const Graph<T>& g, int src, int dst) {
+  int N = g.NumVertices();
+  std::vector<std::vector<T>> flow(N, std::vector<T>(N));
   T total = 0;
   while (true) {
-    queue<pair<int, T>> que;
-    que.push({s, numeric_limits<T>::max()});
-    V<int> prev(g.NumVertices(), -1);
+    std::queue<std::pair<int, T>> que;
+    que.push({src, std::numeric_limits<T>::max()});
+    std::vector<int> prev(N, -1);
     while (!que.empty()) {
-      auto [h, x] = que.front();
+      auto [u, f] = que.front();
       que.pop();
-      if (h == d) {
-        total += x;
-        while (h != s) {
-          flow[prev[h]][h] += x;
-          flow[h][prev[h]] -= x;
-          h = prev[h];
+      if (u == dst) {
+        total += f;
+        while (u != src) {
+          flow[prev[u]][u] += f;
+          flow[u][prev[u]] -= f;
+          u = prev[u];
         }
         break;
       }
-      for (auto& e : g.Edges(h)) {
+      for (auto& e : g.Edges(u)) {
         if (prev[e.to] != -1) continue;
-        T residue = e.weight - flow[e.from][e.to];
+        T residue = e.weight - flow[u][e.to];
         if (residue == 0) continue;
-        prev[e.to] = e.from;
-        que.push({e.to, min(x, residue)});
+        prev[e.to] = u;
+        que.push({e.to, std::min(f, residue)});
       }
     }
-    if (prev[d] == -1) break;
+    if (prev[dst] == -1) break;
   }
   return total;
 }
+
+#include "macros.h"
+
+using namespace std;
 
 struct XY {
   int x, y;
@@ -57,5 +59,5 @@ int main() {
   rep(i, n) add(b0 + i, d);
   rep(i, n) rep(j, n) if (r[i].x < b[j].x && r[i].y < b[j].y)
       add(r0 + i, b0 + j);
-  wt(FordFulkerson(g, s, d));
+  wt(EdmondsKarp(g, s, d));
 }
