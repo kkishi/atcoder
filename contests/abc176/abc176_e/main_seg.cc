0e059c77dfdff0a8b6ddaec9742e42a9bd5852d9
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-22 16:34:21 +0200

    add a solution for abc176/e using segment tree

---
+++ contests/abc176/abc176_e/main_seg.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include "macros.h"
+#include "segment_tree.h"
+
+using namespace std;
+
+int main() {
+  ints(H, W, m);
+  VV<int> hw(H);
+  V<int> hm(H), wm(W);
+  rep(i, m) {
+    ints(h, w);
+    --h, --w;
+    hw[h].push_back(w);
+    hm[h]++;
+    wm[w]++;
+  }
+
+  SegmentTree<int> tree(W, [](int a, int b) { return max(a, b); });
+  rep(i, W) tree.Set(i, wm[i]);
+
+  int ans = 0;
+  rep(i, H) {
+    for (int j : hw[i]) {
+      tree.Set(j, wm[j] - 1);
+    }
+    chmax(ans, hm[i] + tree.Aggregate(0, W));
+    for (int j : hw[i]) {
+      tree.Set(j, wm[j]);
+    }
+  }
+  wt(ans);
+}
