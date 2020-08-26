13797c3dd0207ddb5f71183e9c238e8b7e61e309
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-26 09:25:14 +0200

    use updated grid

--- contests/abc088/abc088_d/main.cc
+++ contests/abc088/abc088_d/main.cc
@@ -11,8 +11,8 @@ int main() {
   cin >> s;
 
   Grid<int> g(h, w);
-  rep(i, h) rep(j, w) g.Set(i, j, (s[i][j] == '#' ? -1 : 1));
-  optional<int> d = g.Distance(0, 0, h - 1, w - 1);
+  rep(i, h) rep(j, w) if (s[i][j] != '#') g.Set(i, j, 1);
+  optional<int> d = g.Distance(0, 0)[h - 1][w - 1];
   if (!d) {
     wt(-1);
   } else {
--- contests/abc151/abc151_d/main.cc
+++ contests/abc151/abc151_d/main.cc
@@ -1,64 +1,22 @@
 #include <bits/stdc++.h>
 
+#include "grid.h"
 #include "macros.h"
 
 using namespace std;
-
-template <typename T>
-class Grid {
- public:
-  Grid(int height, int width) : grid_(height, std::vector<T>(width)) {}
-  void Set(int row, int col, T value) { grid_[row][col] = value; }
-  T Distance(int src_row, int src_col) {
-    if (grid_[src_row][src_col] < 0) {
-      return -1;
-    }
-    low_priority_queue<std::tuple<T, int, int>> que;
-    que.push(make_tuple(0, src_row, src_col));
-    std::map<std::pair<int, int>, T> visited;
-    visited[{src_row, src_col}] = 0;
-    T ret = 0;
-    while (!que.empty()) {
-      T dist;
-      int row, col;
-      std::tie(dist, row, col) = que.top();
-      que.pop();
-      chmax(ret, dist);
-      int dr[] = {1, -1, 0, 0};
-      int dc[] = {0, 0, 1, -1};
-      rep(i, 4) {
-        int nrow = row + dr[i];
-        int ncol = col + dc[i];
-        if (nrow < 0 || grid_.size() <= nrow || ncol < 0 ||
-            grid_[0].size() <= ncol || grid_[nrow][ncol] < 0) {
-          continue;
-        }
-        int ndist = dist + grid_[nrow][ncol];
-        const auto it = visited.find({nrow, ncol});
-        if (it != visited.end() && it->second <= ndist) {
-          continue;
-        }
-        visited[{nrow, ncol}] = ndist;
-        que.push(make_tuple(ndist, nrow, ncol));
-      }
-    }
-    return ret;
-  }
-
- private:
-  std::vector<std::vector<T>> grid_;
-};
 
 int main() {
   ints(H, W);
   vector<string> m(H);
   cin >> m;
 
+  Grid<int> grid(H, W);
+  rep(r, H) rep(c, W) if (m[r][c] == '.') grid.Set(r, c, 1);
+
   int ans = 0;
-  rep(sr, H) rep(sc, W) {
-    Grid<int> grid(H, W);
-    rep(r, H) rep(c, W) grid.Set(r, c, m[r][c] == '.' ? 1 : -1);
-    chmax(ans, grid.Distance(sr, sc));
+  rep(sr, H) rep(sc, W) if (m[sr][sc] == '.') {
+    VV<optional<int>> dist = grid.Distance(sr, sc);
+    rep(r, H) rep(c, W) if (optional<int> d = dist[r][c]; d) chmax(ans, *d);
   }
   wt(ans);
 }
