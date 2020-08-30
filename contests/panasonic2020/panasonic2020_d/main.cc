c6a8c16ba7f9ab9eb7fadb6e78a6d5c1190ebf52
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-30 21:06:45 +0200

    panasonic2020/d

---
+++ contests/panasonic2020/panasonic2020_d/main.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#include "macros.h"
+
+using namespace std;
+
+int main() {
+  ints(n);
+  V<string> v{"a"};
+  rep(n - 1) {
+    V<string> nv;
+    for (const string& s : v) {
+      char mc = 'a';
+      for (char c : s) chmax(mc, c);
+      for (char c = 'a'; c <= mc + 1; ++c) {
+        nv.push_back(s + c);
+      }
+    }
+    swap(v, nv);
+  }
+  for (const string& s : v) wt(s);
+}
