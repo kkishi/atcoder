e6dbf51253be1dd74b5da48d9ab9d8ced35a35b6
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-09-05 14:47:23 +0200

    abc046 d

--- contests/abc046/arc062_b/main.cc
+++ contests/abc046/arc062_b/main.cc
@@ -5,4 +5,18 @@
 using namespace std;
 
 int main() {
+  strings(s);
+  int ans = 0;
+  rep(i, sz(s)) {
+    if (s[i] == 'g') {
+      if (i & 1) {
+        ++ans;
+      }
+    } else if (s[i] == 'p') {
+      if (!(i & 1)) {
+        --ans;
+      }
+    }
+  }
+  wt(ans);
 }
