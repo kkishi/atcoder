8e6b2c2955d28eff80bb86d7276f33fd23491959
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-09-16 16:44:12 +0200

    abc036

---
+++ contests/abc036/abc036_a/main.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+#include "atcoder.h"
+
+void Main() {
+  ints(a, b);
+  wt((b + a - 1) / a);
+}
---
+++ contests/abc036/abc036_d/main.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#include "atcoder.h"
+#include "modint.h"
+
+using mint = ModInt<>;
+
+void Main() {
+  ints(n);
+  VV<int> to(n);
+  rep(n - 1) {
+    ints(a, b);
+    --a, --b;
+    to[a].push_back(b);
+    to[b].push_back(a);
+  }
+  VV<optional<mint>> dp(n, V<optional<mint>>(2));
+  wt(Fix([&](auto rec, int n, int p, bool black) -> mint {
+    optional<mint>& d = dp[n][black];
+    if (!d) {
+      mint w = 1;
+      mint b = black ? 0 : 1;
+      for (int c : to[n]) {
+        if (c == p) continue;
+        w *= rec(c, n, false);
+        b *= rec(c, n, true);
+      }
+      d = w + b;
+    }
+    return *d;
+  })(0, -1, false));
+}
