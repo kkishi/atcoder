3cd5b992fa4f7783998afb027da79e907bba91a8
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-09-19 23:51:00 +0200

    solve scc and mincostflow

--- contests/practice2/practice2_e/main.cc
+++ contests/practice2/practice2_e/main.cc
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 
-#include "macros.h"
+#include <atcoder/mincostflow>
 
-using namespace std;
+#include "atcoder.h"
 
-int main() {
+void Main() {
+  ints(n, k);
+  VV<int> a(n, V<int>(n));
+  cin >> a;
+
+  int s = n * 2, d = s + 1;
+  atcoder::mcf_graph<int, int> g(d + 1);
+  rep(i, n) g.add_edge(s, i, k, 0);
+  rep(i, n) g.add_edge(n + i, d, k, 0);
+  const int big = 1'000'000'000LL;
+  VV<int> e(n, V<int>(n));
+  rep(i, n) rep(j, n) e[i][j] = g.add_edge(i, n + j, 1, big - a[i][j]);
+  g.add_edge(s, d, n * k, big);
+
+  auto [cap, cost] = g.flow(s, d, n * k);
+  wt(cap * big - cost);
+  rep(i, n) {
+    rep(j, n) cout << (g.get_edge(e[i][j]).flow ? 'X' : '.');
+    cout << endl;
+  }
 }
--- contests/practice2/practice2_g/main.cc
+++ contests/practice2/practice2_g/main.cc
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 
-#include "macros.h"
+#include "atcoder.h"
+#include "fast_io.h"
+#include "graph.h"
+#include "strongly_connected_component.h"
+#include "topological_sort.h"
 
-using namespace std;
+void Main() {
+  ints(n, m);
+  Graph<int> g(n);
+  rep(m) {
+    ints(a, b);
+    g.AddEdge(a, b);
+  }
 
-int main() {
+  VV<int> scc = StronglyConnectedComponents(g);
+  V<int> scc_index(n);
+  rep(i, scc.size()) for (int u : scc[i]) scc_index[u] = i;
+
+  Graph<int> h(scc.size());
+  rep(u, n) for (const auto& e : g.Edges(u)) {
+    int v = e.to;
+    int ui = scc_index[u], vi = scc_index[v];
+    if (ui != vi) h.AddEdge(ui, vi);
+  }
+  V<int> ts = TopologicalSort(h);
+
+  wt(scc.size());
+  for (int i : ts) {
+    cout << scc[i].size();
+    for (int j : scc[i]) cout << " " << j;
+    cout << endl;
+  }
 }
