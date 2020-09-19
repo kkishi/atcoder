f944eacd79172197e5c53e0e3bbabdb824457ee2
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-09-20 01:38:54 +0200

    solve lazy segment tree problems

--- contests/practice2/practice2_k/main.cc
+++ contests/practice2/practice2_k/main.cc
@@ -1,8 +1,47 @@
 #include <bits/stdc++.h>
 
-#include "macros.h"
+#include <atcoder/lazysegtree>
 
-using namespace std;
+#include "atcoder.h"
+#include "modint.h"
 
-int main() {
+using mint = ModInt<998244353>;
+
+struct S {
+  mint val;
+  int len;
+};
+
+S op(S a, S b) { return {a.val + b.val, a.len + b.len}; }
+
+S e() { return {0, 0}; }
+
+struct F {
+  mint b, c;
+};
+
+S mapping(F f, S x) { return {f.b * x.val + f.c * x.len, x.len}; }
+
+F composition(F f, F g) { return {f.b * g.b, f.b * g.c + f.c}; }
+
+F id() { return {1, 0}; }
+
+void Main() {
+  ints(n, q);
+  V<int> a(n);
+  cin >> a;
+
+  atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> seg(n);
+  rep(i, n) seg.set(i, {a[i], 1});
+
+  rep(q) {
+    ints(k);
+    if (k == 0) {
+      ints(l, r, b, c);
+      seg.apply(l, r, {b, c});
+    } else {
+      ints(l, r);
+      wt(seg.prod(l, r).val);
+    }
+  }
 }
