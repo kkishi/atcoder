37b324b35427e1b0c9549f3885aa7e70a3c4854f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-10-09 10:20:31 +0200

    add solutions

--- contests/dp/dp_l/main.cc
+++ contests/dp/dp_l/main.cc
@@ -6,19 +6,16 @@ void Main() {
   ints(n);
   V<int> a(n);
   cin >> a;
-  vector dp(2, vector(n, vector(n, optional<int>())));
-  wt(Fix([&](auto rec, int l, int r, bool taro) -> int {
-    auto& d = dp[taro][l][r];
+  vector dp(n, vector(n, optional<int>()));
+  wt(Fix([&](auto rec, int l, int r) -> int {
+    auto& d = dp[l][r];
     if (!d) {
       if (l == r) {
-        d = taro ? a[l] : -a[l];
+        d = a[l];
       } else {
-        int i = rec(l + 1, r, !taro) + (taro ? a[l] : -a[l]);
-        int j = rec(l, r - 1, !taro) + (taro ? a[r] : -a[r]);
-        if (taro != (i > j)) swap(i, j);
-        d = i;
+        d = max(a[l] - rec(l + 1, r), a[r] - rec(l, r - 1));
       }
     }
     return *d;
-  })(0, n - 1, true));
+  })(0, n - 1));
 }
--- contests/dp/dp_m/main.cc
+++ contests/dp/dp_m/main.cc
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
+#include "modint.h"
+
+using mint = ModInt<>;
 
 void Main() {
+  ints(n, k);
+  V<int> a(n);
+  cin >> a;
+  vector dp(n, vector(k + 1, mint(0)));
+  vector sdp(n, vector(k + 1, mint(0)));
+  rep(i, k + 1) dp[0][i] = i <= a[0];
+  sdp[0][0] = dp[0][0];
+  rep(i, 1, k + 1) sdp[0][i] = sdp[0][i - 1] + dp[0][i];
+  rep(i, 1, n) {
+    rep(j, k + 1) {
+      int l = j - a[i] - 1;
+      dp[i][j] += sdp[i - 1][j] - (l >= 0 ? sdp[i - 1][l] : 0);
+    }
+    sdp[i][0] = dp[i][0];
+    rep(j, 1, k + 1) sdp[i][j] = sdp[i][j - 1] + dp[i][j];
+  }
+  wt(dp[n - 1][k]);
 }
