7c3a1d5432d074b278207fd1a0fb3ee08e7ddf5b
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-10-09 10:37:18 +0200

    add solutions

--- contests/dp/dp_n/main.cc
+++ contests/dp/dp_n/main.cc
@@ -10,16 +10,13 @@ void Main() {
   rep(i, n) cum[i + 1] = cum[i] + a[i];
   vector dp(n + 1, vector(n + 1, optional<int>()));
   wt(Fix([&](auto rec, int begin, int end) -> int {
+    if (end - begin == 1) return 0;
     auto& d = dp[begin][end];
     if (!d) {
-      d = [&]() -> int {
-        if (end - begin == 1) return 0;
-        int ret = numeric_limits<int>::max();
-        for (int mid = begin + 1; mid < end; ++mid) {
-          chmin(ret, rec(begin, mid) + rec(mid, end) + cum[end] - cum[begin]);
-        }
-        return ret;
-      }();
+      d = numeric_limits<int>::max();
+      for (int mid = begin + 1; mid < end; ++mid) {
+        chmin(d, rec(begin, mid) + rec(mid, end) + cum[end] - cum[begin]);
+      }
     }
     return *d;
   })(0, n));
--- contests/dp/dp_o/main.cc
+++ contests/dp/dp_o/main.cc
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
+#include "modint.h"
+
+using mint = ModInt<>;
 
 void Main() {
+  ints(n);
+  VV<int> a(n, V<int>(n));
+  cin >> a;
+  VV<mint> dp(n + 1, V<mint>(1 << n));
+  dp[0][0] = 1;
+  rep(s, 1 << n) {
+    int i = popcount(s);
+    rep(j, n) if (int b = 1 << j; !(s & b) && a[i][j]) {
+      dp[i + 1][s | b] += dp[i][s];
+    }
+  }
+  wt(dp[n][(1 << n) - 1]);
 }
