2fc474a6d08f3a2f729bedbb9c22c21b79641ca0
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-10-11 23:00:09 +0200

    simplify

--- contests/dp/dp_u/main.cc
+++ contests/dp/dp_u/main.cc
@@ -6,17 +6,10 @@ void Main() {
   ints(n);
   VV<int> a(n, V<int>(n));
   cin >> a;
-  V<optional<int>> dp(1 << n);
-  auto bit = [](int s, int i) { return (s >> i) & 1; };
-  wt(Fix([&](auto rec, int s) -> int {
-    auto& d = dp[s];
-    if (!d) {
-      d = 0;
-      rep(i, 1, n) rep(j, i) if (bit(s, i) && bit(s, j))* d += a[i][j];
-      for (int t = (s - 1) & s; t > 0; t = (t - 1) & s) {
-        chmax(*d, rec(t) + rec(s ^ t));
-      }
-    }
-    return *d;
-  })((1 << n) - 1));
+  V<int> dp(1 << n);
+  rep(s, 1 << n) {
+    rep(i, 1, n) rep(j, i) if ((s >> i) & (s >> j) & 1) dp[s] += a[i][j];
+    for (int t = s; t > 0; t = (t - 1) & s) chmax(dp[s], dp[t] + dp[s - t]);
+  }
+  wt(dp[(1 << n) - 1]);
 }
