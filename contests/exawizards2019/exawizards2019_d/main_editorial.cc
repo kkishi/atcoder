c5c58fdc81b204a31be4f5f02de232d13452856d
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-10-12 23:56:49 +0200

    correct the editorial solution

--- contests/exawizards2019/exawizards2019_d/main_editorial.cc
+++ contests/exawizards2019/exawizards2019_d/main_editorial.cc
@@ -10,13 +10,18 @@ void Main() {
   V<int> s(n);
   cin >> s;
   sort(all(s), greater());
-  vector dp(n + 1, vector(x + 1, mint(0)));
-  dp[0][x] = 1;
-  rep(i, n) rep(j, x + 1) {
-    dp[i + 1][j % s[i]] += dp[i][j] / (n - i);
-    dp[i + 1][j] += dp[i][j] * (n - 1 - i) / (n - i);
-  }
-  mint ans = 0;
-  rep(i, x + 1) ans += dp[n][i] * i * mint::Fact(n);
-  wt(ans);
+  vector dp(n + 1, vector(x + 1, optional<mint>()));
+  wt(Fix([&](auto rec, int i, int y) -> mint {
+       auto& d = dp[i][y];
+       if (!d) {
+         if (i == n) {
+           d = y;
+         } else {
+           mint rem = n - i;
+           d = 1 / rem * rec(i + 1, y % s[i]) + (rem - 1) / rem * rec(i + 1, y);
+         }
+       }
+       return *d;
+     })(0, x) *
+     mint::Fact(n));
 }
