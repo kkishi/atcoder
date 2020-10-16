692853afa9f0bb1cbed0bfe69a4d33fbe8cae2ea
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-10-16 17:57:31 +0200

    add solutions

--- contests/abc048/arc064_b/main.cc
+++ contests/abc048/arc064_b/main.cc
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 
-#include "macros.h"
+#include "atcoder.h"
 
-using namespace std;
-
-int main() {}
+void Main() {
+  strings(s);
+  int x = sz(s);
+  if (s[0] == s.back()) {
+    x -= 3;
+  } else {
+    x -= 2;
+  }
+  if (x & 1) {
+    wt("First");
+  } else {
+    wt("Second");
+  }
+}
--- contests/dp/dp_x/main.cc
+++ contests/dp/dp_x/main.cc
@@ -3,4 +3,20 @@
 #include "atcoder.h"
 
 void Main() {
+  ints(n);
+  V<tuple<int, int, int, int>> b;
+  rep(n) {
+    ints(w, s, v);
+    b.emplace_back(s + w, w, s, v);
+  }
+  sort(all(b));
+  V<int> dp(20001, -1);
+  dp[0] = 0;
+  for (auto [_, w, s, v] : b) {
+    for (int i = s; i >= 0; --i) {
+      if (dp[i] == -1) continue;
+      chmax(dp[i + w], dp[i] + v);
+    }
+  }
+  wt(*max_element(all(dp)));
 }
