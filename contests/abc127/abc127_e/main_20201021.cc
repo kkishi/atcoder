81f21cbcc22e8f7ef43a6a7401e184aed810efb0
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-10-21 11:02:34 +0200

    simplify

--- contests/abc127/abc127_e/main_20201021.cc
+++ contests/abc127/abc127_e/main_20201021.cc
@@ -12,7 +12,7 @@ void Main() {
     mint x = 0;
     rep(i, 1, m) x += i * (m - i);
     ans += mint::Comb(k, 2) * x * 2 * mint(n).Pow(2) *
-           mint::Comb(n * m - 2, k - 2) * mint::Fact(k - 2) / mint::Fact(k);
+           mint::Perm(n * m - 2, k - 2) / mint::Fact(k);
     swap(n, m);
   }
   wt(ans);
