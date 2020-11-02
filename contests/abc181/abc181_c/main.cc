a6343e3bfa3b8f3389c8c7b5d4ba443ac8c6c967
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-11-02 13:22:39 +0100

    solve abc149f

--- contests/abc181/abc181_c/main.cc
+++ contests/abc181/abc181_c/main.cc
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
+#include "geometry.h"
+
+using Vec = Vector<int>;
 
 void Main() {
   ints(n);
-  V<int> x(n), y(n);
-  rep(i, n) cin >> x[i] >> y[i];
+  V<Vec> v(n);
+  cin >> v;
   bool ok = false;
   rep(i, n) rep(j, i + 1, n) rep(k, j + 1, n) {
-    int a = x[j] - x[i];
-    int b = x[k] - x[i];
-    int c = y[j] - y[i];
-    int d = y[k] - y[i];
-    if (a * d == c * b) ok = true;
+    if (Cross(v[j] - v[i], v[k] - v[i]) == 0) ok = true;
   }
   wt(ok);
 }
