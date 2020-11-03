50ebf1d1099d19a55db9a5fe74d2d9c38e9ae5cc
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-11-03 20:49:22 +0100

    add solutions

---
+++ contests/agc005/agc005_b/main.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include "atcoder.h"
+
+void Main() {
+  ints(n);
+  V<pair<int, int>> v;
+  rep(i, n) {
+    ints(a);
+    v.emplace_back(a, i);
+  }
+  sort(all(v));
+  set<int> s = {-1, n};
+  int ans = 0;
+  for (auto [ai, i] : v) {
+    auto [it, _] = s.insert(i);
+    ans += (i - *prev(it)) * (*next(it) - i) * ai;
+  }
+  wt(ans);
+}
