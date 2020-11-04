15b783f51b5a539fc7fc2727d2ef7f4be1188fda
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-11-04 12:25:52 +0100

    add solutions

---
+++ contests/arc031/arc031_2/main.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#include "atcoder.h"
+#include "disjointset.h"
+
+void Main() {
+  V<string> a(10);
+  cin >> a;
+  bool ok = false;
+  rep(i, 10) rep(j, 10) if (a[i][j] == 'x') {
+    a[i][j] = 'o';
+    DisjointSet ds(100);
+    auto idx = [](int r, int c) { return r * 10 + c; };
+    rep(k, 10) rep(l, 10) if (a[k][l] == 'o') {
+      if (k + 1 < 10 && a[k + 1][l] == 'o') ds.Union(idx(k, l), idx(k + 1, l));
+      if (l + 1 < 10 && a[k][l + 1] == 'o') ds.Union(idx(k, l), idx(k, l + 1));
+    }
+    set<int> s;
+    rep(k, 10) rep(l, 10) if (a[k][l] == 'o') s.insert(ds.Find(idx(k, l)));
+    if (sz(s) == 1) ok = true;
+    a[i][j] = 'x';
+  }
+  wt(ok ? "YES" : "NO");
+}
---
+++ contests/arc031/arc031_3/main.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#include "atcoder.h"
+#include "bit.h"
+
+void Main() {
+  ints(n);
+
+  V<pair<int, int>> bs;
+  rep(i, n) {
+    ints(b);
+    bs.emplace_back(b - 1, i);
+  }
+  sort(all(bs), greater());
+
+  int ans = 0;
+  BIT<int> bit(n);
+  for (auto [b, i] : bs) {
+    ans += min(bit.Sum(i - 1), bit.Sum(n - 1) - bit.Sum(i));
+    bit.Add(i, 1);
+  }
+  wt(ans);
+}
