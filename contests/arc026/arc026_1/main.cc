6f0256109f1c5f3189812f09d23d822bf7dd6388
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-12-22 03:22:57 +0100

    add solutions

--- contests/arc026/arc026_1/main.cc
+++ contests/arc026/arc026_1/main.cc
@@ -3,4 +3,6 @@
 #include "atcoder.h"
 
 void Main() {
+  ints(n, a, b);
+  wt(min(5, n) * b + max(0, n - 5) * a);
 }
--- contests/arc026/arc026_2/main.cc
+++ contests/arc026/arc026_2/main.cc
@@ -3,4 +3,15 @@
 #include "atcoder.h"
 
 void Main() {
+  ints(n);
+  set<int> s;
+  for (int i = 1; i * i <= n; ++i) {
+    if (n % i == 0) {
+      s.insert(i);
+      s.insert(n / i);
+    }
+  }
+  s.erase(n);
+  int sum = accumulate(all(s), int(0));
+  wt(sum == n ? "Perfect" : sum < n ? "Deficient" : "Abundant");
 }
