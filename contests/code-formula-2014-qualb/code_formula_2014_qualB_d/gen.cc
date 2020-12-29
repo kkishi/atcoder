97ea92a21ae4324730032ae6f14168782e4cb233
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-12-29 01:29:25 +0100

    add solutions

---
+++ contests/code-formula-2014-qualb/code_formula_2014_qualB_d/gen.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+#include "atcoder.h"
+#include "rand.h"
+
+void Main() {
+  int n = 4;
+  wt(n);
+  Random r;
+  rep(n) wt(r.Int(0, 100));
+}
---
+++ contests/code-formula-2014-qualb/code_formula_2014_qualB_d/naive.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#include "atcoder.h"
+#include "modint.h"
+
+using mint = ModInt<>;
+
+void Main() {
+  ints(n);
+  V<int> a(n);
+  cin >> a;
+
+  set<int> s;
+  Fix([&](auto rec, int depth, int x) {
+    if (depth == n) {
+      s.insert(x);
+      return;
+    }
+    int ten = 1;
+    rep(depth) ten *= 10;
+    rep(i, a[depth] + 1) {
+      rec(depth + 1, x);
+      x += ten;
+    }
+  })(0, 0);
+  wt(sz(s) - 1);
+}
