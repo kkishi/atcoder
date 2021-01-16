ec8279b36acad2a699c533dae17d6c7675c9aa0f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-01-16 01:33:13 +0100

    add solution

--- contests/arc106/arc106_d/main.cc
+++ contests/arc106/arc106_d/main.cc
@@ -9,17 +9,22 @@ void Main() {
   ints(n, k);
   V<int> a(n);
   cin >> a;
-
-  mint sum = 0;
-  rep(i, n) sum += a[i];
-
-  mint x = 1;
-  V<int> b(n, 1);
-  rep(i, k) {
-    x *= sum;
-    rep(j, n) b[j] *= a[j];
-    mint y = 0;
-    rep(j, n) y += b[j];
-    wt(x + y);
+  vector b(k + 1, vector(n, mint(1)));
+  rep(i, k) rep(j, n) b[i + 1][j] = b[i][j] * a[j];
+  V<mint> s(k + 1);
+  rep(i, k + 1) s[i] = accumulate(all(b[i]), mint(0));
+  vector comb(k + 1, vector(k + 1, mint()));
+  rep(i, k + 1) rep(j, k + 1) comb[i][j] = mint::Comb(i, j);
+  rep(i, 1, k + 1) {
+    mint sum = 0;
+    rep(j, i + 1) sum += comb[i][j] * (s[i - j] * s[j] - s[i]);
+    wt(sum / 2);
+#ifdef DEBUG
+    mint slow = 0;
+    rep(l, n) {
+      rep(j, i + 1) slow += comb[i][j] * b[i - j][l] * (s[j] - b[j][l]);
+    }
+    assert(sum == slow);
+#endif
   }
 }
