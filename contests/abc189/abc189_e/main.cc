907f79102f8345218df854ceba5f05be433ca914
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-01-24 03:18:45 +0100

    simplify

--- contests/abc189/abc189_e/main.cc
+++ contests/abc189/abc189_e/main.cc
@@ -14,42 +14,35 @@ void Main() {
     P.pb({{{x}, {y}}});
   }
   ints(m);
-  V<int> k(m), p(m);
+  V<vec> C(m + 1);
+  C[0] = {{{0}, {0}}};
+  V<mat> A(m + 1);
+  A[0] = {{{1, 0}, {0, 1}}};
   rep(i, m) {
-    cin >> k[i];
-    if (k[i] >= 3) cin >> p[i];
+    ints(k);
+    if (k == 1) {
+      mat x = {{{0, 1}, {-1, 0}}};
+      A[i + 1] = Mult(x, A[i]);
+      C[i + 1] = Mult(x, C[i]);
+    } else if (k == 2) {
+      mat x = {{{0, -1}, {1, 0}}};
+      A[i + 1] = Mult(x, A[i]);
+      C[i + 1] = Mult(x, C[i]);
+    } else {
+      ints(p);
+      if (k == 3) {
+        C[i + 1] = {{{2 * p - C[i][0][0]}, {C[i][1][0]}}};
+        A[i + 1] = Mult(mat{{{-1, 0}, {0, 1}}}, A[i]);
+      } else {
+        C[i + 1] = {{{C[i][0][0]}, {2 * p - C[i][1][0]}}};
+        A[i + 1] = Mult(mat{{{1, 0}, {0, -1}}}, A[i]);
+      }
+    }
   }
   ints(q);
-  VV<pair<int, int>> ab(m + 1);
-  rep(i, q) {
+  rep(q) {
     ints(a, b);
-    ab[a].pb({i, b - 1});
+    vec ans = Plus(Mult(A[a], P[b - 1]), C[a]);
+    wt(ans[0][0], ans[1][0]);
   }
-
-  vec C = {{{0}, {0}}};
-  mat A = {{{1, 0}, {0, 1}}};
-  V<vec> ans(q);
-  rep(i, m + 1) {
-    each(j, b, ab[i]) ans[j] = Plus(Mult(A, P[b]), C);
-    if (i == m) break;
-    if (k[i] == 1) {
-      mat x = {{{0, 1}, {-1, 0}}};
-      A = Mult(x, A);
-      C = Mult(x, C);
-    }
-    if (k[i] == 2) {
-      mat x = {{{0, -1}, {1, 0}}};
-      A = Mult(x, A);
-      C = Mult(x, C);
-    }
-    if (k[i] == 3) {
-      C[0][0] = 2 * p[i] - C[0][0];
-      A = Mult(mat{{{-1, 0}, {0, 1}}}, A);
-    }
-    if (k[i] == 4) {
-      C[1][0] = 2 * p[i] - C[1][0];
-      A = Mult(mat{{{1, 0}, {0, -1}}}, A);
-    }
-  }
-  rep(i, q) wt(ans[i][0][0], ans[i][1][0]);
 }
