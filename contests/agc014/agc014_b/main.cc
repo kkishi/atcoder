57ee72fbe316e8b571975115e1e1defe8449ce8b
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-02-02 09:43:23 +0100

    add solutions

--- contests/agc014/agc014_b/main.cc
+++ contests/agc014/agc014_b/main.cc
@@ -3,4 +3,16 @@
 #include "atcoder.h"
 
 void Main() {
+  ints(n, m);
+  V<int> cnt(n);
+  rep(m) {
+    ints(a, b);
+    ++cnt[a - 1];
+    ++cnt[b - 1];
+  }
+  each(c, cnt) if (!even(c)) {
+    wt("NO");
+    return;
+  }
+  wt("YES");
 }
