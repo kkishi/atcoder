0631e51a71cc56f13ba4b853310de9f8f4e3bedb
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-02-13 16:00:38 +0100

    1 dfs

--- contests/arc112/arc112_c/main.cc
+++ contests/arc112/arc112_c/main.cc
@@ -9,24 +9,19 @@ void Main() {
     ints(p);
     to[p - 1].pb(i);
   }
-  V<int> size(n);
-  Fix([&](auto rec, int node) -> int {
-    int ret = 1;
-    each(child, to[node]) ret += rec(child);
-    size[node] = ret;
-    return ret;
-  })(0);
-  int res = Fix([&](auto rec, int node) -> int {
+  auto [res, _] = Fix([&](auto rec, int node) -> pair<int, int> {
     V<int> e, o;
+    int size = 1;
     each(child, to[node]) {
-      int opp = rec(child);
-      (even(size[child]) ? e : o).pb(-opp);
+      auto [opp, csize] = rec(child);
+      (even(csize) ? e : o).pb(-opp);
+      size += csize;
     }
-    sort(all(o), greater());
     int ret = 0;
     each(x, e) ret += (x >= 0 || even(sz(o))) ? x : -x;
+    sort(all(o), greater());
     rep(i, sz(o)) ret += even(i) ? o[i] : -o[i];
-    return 1 - ret;
+    return {1 - ret, size};
   })(0);
   wt((res + n) / 2);
 }
