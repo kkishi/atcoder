eb4a9073017d6ceab86136cd69ddccc369eeb73a
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-04-01 14:40:25 +0200

    add solutions

--- contests/past202004-open/past202004_i/main.cc
+++ contests/past202004-open/past202004_i/main.cc
@@ -3,4 +3,24 @@
 #include "atcoder.h"
 
 void Main() {
+  ints(n);
+  V<pair<int, int>> a(1 << n);
+  rep(i, 1 << n) {
+    a[i].first = i;
+    cin >> a[i].second;
+  }
+  V<int> ans(1 << n);
+  Fix([&](auto rec, int begin, int end, int x) -> pair<int, int> {
+    if (end - begin == 1) return a[begin];
+    int mid = (begin + end) / 2;
+    auto [li, lv] = rec(begin, mid, x - 1);
+    auto [ri, rv] = rec(mid, end, x - 1);
+    ans[li] = ans[ri] = x;
+    if (lv < rv) {
+      return {ri, rv};
+    } else {
+      return {li, lv};
+    }
+  })(0, 1 << n, n);
+  each(e, ans) wt(e);
 }
--- contests/past202004-open/past202004_j/main.cc
+++ contests/past202004-open/past202004_j/main.cc
@@ -3,4 +3,24 @@
 #include "atcoder.h"
 
 void Main() {
+  strings(s);
+  auto f = [&](int begin, int end) { return s.substr(begin, end - begin); };
+  wt(Fix([&](auto rec, int begin, int end) -> pair<string, int> {
+       string ret;
+       int b = begin;
+       while (b < end) {
+         int i = b;
+         while (i < end && isalpha(s[i])) ++i;
+         if (i == end) return {ret + f(b, end), end};
+         if (s[i] == ')') return {ret + f(b, i), i + 1};
+         assert(s[i] == '(');
+         auto [res, pos] = rec(i + 1, end);
+         string ser = res;
+         reverse(all(ser));
+         ret += f(b, i) + res + ser;
+         b = pos;
+       }
+       return {ret, end};
+     })(0, sz(s))
+         .first);
 }
