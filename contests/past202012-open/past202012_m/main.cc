a06307572084b39398226f76b3d7e657e7ae564f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-04-16 01:31:07 +0200

    use faster segment tree

--- contests/past202012-open/past202012_m/main.cc
+++ contests/past202012-open/past202012_m/main.cc
@@ -1,13 +1,8 @@
 #include <bits/stdc++.h>
 
-#include <atcoder/segtree>
-
 #include "atcoder.h"
 #include "binary_search.h"
-// #include "segment_tree.h"
-
-int op(int a, int b) { return max(a, b); }
-int e() { return 0; }
+#include "segment_tree.h"
 
 void Main() {
   ints(n, l);
@@ -16,21 +11,17 @@ void Main() {
   V<int> s(n + 1);
   rep(i, n) s[i + 1] = s[i] + a[i];
   wt(BinarySearch<int>(0, l + 1, [&](int x) {
-    // SegmentTree<int> t(n + 1, [](int a, int b) { return max(a, b); });
-    atcoder::segtree<int, op, e> t(n + 1);
+    SegmentTree<int> t(n + 1, [](int a, int b) { return max(a, b); });
     int lo = 0, hi = 0;
     rep(i, n) {
       while (s[i + 1] - s[lo] > l) ++lo;
       while (s[i + 1] - s[hi] >= x) ++hi;
       if (lo == 0) {
-        // t.Set(i + 1, s[i + 1]);
-        t.set(i + 1, s[i + 1]);
+        t.Set(i + 1, s[i + 1]);
       } else {
-        // t.Set(i + 1, t.Aggregate(lo, hi));
-        t.set(i + 1, t.prod(lo, hi));
+        t.Set(i + 1, t.Aggregate(lo, hi));
       }
     }
-    // return t.Get(n) >= x;
-    return t.prod(n, n + 1) >= x;
+    return t.Get(n) >= x;
   }));
 }
