4bd51179dddf7184b202bc73307ffda554217e3e
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-05-15 23:31:31 +0200

    add solutions

--- contests/abc200/abc200_e/main_editorial.cc
+++ contests/abc200/abc200_e/main_editorial.cc
@@ -4,30 +4,18 @@
 
 void Main() {
   ints(n, k);
-  int N = n;
-  auto F = [](int n, int m) {
-    if (n < m) return int(0);
-    int p = 1, q = 1;
-    rep(i, m) {
-      p *= n - i;
-      q *= m - i;
-      int g = gcd(p, q);
-      p /= g;
-      q /= g;
+  vector dp(4, vector(4 * n + 1, int(0)));
+  dp[0][0] = 1;
+  rep(i, 3) {
+    rep(j, 3 * n + 1) {
+      dp[i + 1][j + 1] += dp[i][j];
+      dp[i + 1][j + n + 1] -= dp[i][j];
     }
-    assert(q == 1);
-    return p;
-  };
-  auto C = [&F, &N](int n, int m) {
-    int f = F(n, m);
-    int ng = 0;
-    ng += F(n - N, m) * 3;
-    ng -= F(n - N * 2, m) * 3;
-    return f - ng;
-  };
+    rep(j, 3 * n) dp[i + 1][j + 1] += dp[i + 1][j];
+  }
   int x = 3;
   while (true) {
-    int cnt = C(x - 1, 2);
+    int cnt = dp[3][x];
     if (cnt >= k) break;
     k -= cnt;
     ++x;
---
+++ contests/abc201/abc201_e/main_editorial.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#include "atcoder.h"
+#include "modint.h"
+
+using mint = ModInt<>;
+
+void Main() {
+  ints(n);
+  VV<pair<int, int>> g(n);
+  rep(n - 1) {
+    ints(u, v, w);
+    --u, --v;
+    g[u].eb(v, w);
+    g[v].eb(u, w);
+  }
+  V<int> v(n);
+  Fix([&](auto rec, int node, int parent, int value) -> void {
+    v[node] = value;
+    each(child, weight, g[node]) if (child != parent) {
+      rec(child, node, value ^ weight);
+    }
+  })(0, -1, 0);
+  mint ans = 0;
+  rep(b, 60) {
+    mint one = 0, zero = 0;
+    rep(i, n)(hasbit(v[i], b) ? one : zero) += 1;
+    ans += mint(2).Pow(b) * (one * zero);
+  }
+  wt(ans);
+}
