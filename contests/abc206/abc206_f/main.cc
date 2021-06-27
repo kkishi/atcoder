b2682af84304fc4257b81b4cab3a1f7965fedcef
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-06-27 23:24:53 +0200

    add solutions

--- contests/abc206/abc206_f/main.cc
+++ contests/abc206/abc206_f/main.cc
@@ -1,4 +1,3 @@
-// This solution is no correct.
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
@@ -11,19 +10,14 @@ void Solve() {
   rep(i, n) cin >> l[i] >> r[i];
   V<P> seg;
   rep(i, n) seg.eb(l[i], r[i]);
-  dbg(seg);
-#define canw(x) hasbit(x, 0)
-#define canl(x) hasbit(x, 1)
   vector dp(200, vector(200, int(-1)));
   auto rec = Fix([&](auto rec, int l, int r, const V<P>& seg) {
     int& d = dp[l][r];
     if (d != -1) return d;
     if (seg.empty()) {
-      d = 2;  // Lose
-    } else if (sz(seg) == 1) {
-      d = 1;  // Win
-    } else {
       d = 0;
+    } else {
+      set<int> st;
       rep(i, sz(seg)) {
         V<P> L, R;
         rep(j, sz(seg)) {
@@ -32,28 +26,18 @@ void Solve() {
         }
         int ld = rec(l, seg[i].first, L);
         int rd = rec(seg[i].second, r, R);
-        if (!canw(ld) && !canw(rd)) {
-          d |= 1;
-        }
-        if (!canl(ld) && !canl(rd)) {
-          d |= 1;
-        }
-        if (!canl(ld) && !canw(rd)) {
-          d |= 2;
-        }
-        if (!canw(ld) && !canl(rd)) {
-          d |= 2;
-        }
-        if (ld == 3 && rd == 3) {
-          d |= 1;
-        } else if (ld == 3 || rd == 3) {
-          d |= 2;
+        st.insert(ld ^ rd);
+      }
+      for (int i = 0;; ++i) {
+        if (st.count(i) == 0) {
+          d = i;
+          break;
         }
       }
     }
     return d;
   });
-  wt(canw(rec(0, 101, seg)) ? "Alice" : "Bob");
+  wt(rec(0, 101, seg) ? "Alice" : "Bob");
 }
 
 void Main() {
