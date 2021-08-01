2d96bb443bd6be2880d54d842111f390d72a8626
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-08-01 08:25:14 +0200

    add solutions

--- contests/abc212/abc212_g/main.cc
+++ contests/abc212/abc212_g/main.cc
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
+#include "divisors.h"
+#include "modint.h"
+
+using mint = ModInt<998244353>;
 
 void Main() {
+  ints(p);
+  mint ans = 1;
+  int P = p - 1;
+  V<int> d = Divisors(P);
+  map<int, mint> dp;
+  rrep(i, sz(d)) {
+    int D = d[i];
+    dp[D] = P / D;
+    rep(j, i + 1, sz(d)) {
+      int E = d[j];
+      if (E % D == 0) {
+        dp[D] -= dp[E];
+      }
+    }
+  }
+  each(k, v, dp) ans += P / k * v;
+  wt(ans);
 }
--- contests/abc212/abc212_g/naive.cc
+++ contests/abc212/abc212_g/naive.cc
@@ -1,38 +1,32 @@
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
-#include "primes.h"
 
 int Solve(int p) {
   int ans = 0;
-  map<int, int> m;
-  rep(x, p) {
+  rep(x, p) rep(y, p) {
+    bool ok = false;
     int z = x == 0 ? 0 : 1;
     set<int> st;
     while (true) {
       if (st.count(z)) break;
       st.insert(z);
+      if (z == y) {
+        ok = true;
+        break;
+      }
       z *= x;
       z %= p;
     }
-    dbg(p, x, sz(st));
-    ans += sz(st);
-    ++m[sz(st)];
+    if (ok) {
+      dbg(x, y);
+      ++ans;
+    }
   }
-  dbg(p, m);
   return ans;
 }
 
 void Main() {
   ints(p);
-  // wt(Solve(p));
-  // return;
-  V<int> ps = Primes(200);
-  V<int> v;
-  each(e, ps) {
-    int f = Solve(e);
-    dbg(e, f);
-    v.pb(f);
-  }
-  dbg(v);
+  wt(Solve(p));
 }
