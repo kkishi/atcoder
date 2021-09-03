2fbb4ce1607177e2a27ad3628fa2c4b9f8f46dad
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-09-03 21:49:42 +0200

    Use sparse table. This improves the runtime from 869ms to 213ms.

--- contests/abc141/abc141_e/main_sa.cc
+++ contests/abc141/abc141_e/main_sa.cc
@@ -3,7 +3,7 @@
 #include <atcoder/string>
 
 #include "atcoder.h"
-#include "min_segment_tree.h"
+#include "sparse_table.h"
 
 void Main() {
   ints(n);
@@ -12,14 +12,12 @@ void Main() {
   V<int> r(n);
   rep(i, n) r[sa[i]] = i;
   auto la = atcoder::lcp_array(s, sa);
-  int m = sz(la);
-  MinSegmentTree<int> t(m);
-  rep(i, m) t.Set(i, la[i]);
+  SparseTable<i32> t(la, [](int a, int b) { return min(a, b); });
   int ans = 0;
   auto lcp = [&](int i, int j) {
     i = r[i], j = r[j];
     if (i > j) swap(i, j);
-    return t.Aggregate(i, j);
+    return t.Get(i, j);
   };
   rep(i, n - 1) rep(j, i + 1, n) chmax(ans, min(lcp(i, j), j - i));
   wt(ans);
