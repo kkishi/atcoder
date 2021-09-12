2795a60580d867bdd07c25dc9edf58c393618f0d
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-09-12 10:53:19 +0200

    add solutions

---
+++ contests/abc218/abc218_g/main_pbds.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <pbds.h>
+
+#include "atcoder.h"
+
+void Main() {
+  ints(n);
+  V<int> a(n);
+  cin >> a;
+  VV<int> g(n);
+  rep(n - 1) {
+    ints(u, v);
+    --u, --v;
+    g[u].pb(v);
+    g[v].pb(u);
+  }
+  Set<pair<int, int>> s;
+  wt(Fix([&](auto rec, int node, int parent, int depth) -> int {
+    s.insert({a[node], node});
+    int ret = even(depth) ? -big : big;
+    bool leaf = true;
+    each(child, g[node]) {
+      if (child == parent) continue;
+      leaf = false;
+      int r = rec(child, node, depth + 1);
+      if (even(depth)) {
+        chmax(ret, r);
+      } else {
+        chmin(ret, r);
+      }
+    }
+    if (leaf) {
+      auto it = s.find_by_order((sz(s) - 1) / 2);
+      if (even(sz(s))) {
+        ret = (it->first + next(it)->first) / 2;
+      } else {
+        ret = it->first;
+      }
+    }
+    s.erase({a[node], node});
+    return ret;
+  })(0, -1, 0));
+}
