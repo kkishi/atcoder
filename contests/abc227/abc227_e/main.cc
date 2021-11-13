a35cf39c62f6222e15eb5b78133e44971b9599f1
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-11-13 22:43:01 +0100

    add solutions

--- contests/abc227/abc227_e/main.cc
+++ contests/abc227/abc227_e/main.cc
@@ -3,4 +3,26 @@
 #include "atcoder.h"
 
 void Main() {
+  strings(s);
+  each(e, s) e = e == 'K' ? 0 : e == 'E' ? 1 : 2;
+  int n = sz(s);
+  vector dp(n + 1, vector(n * n, vector(n + 1, vector(n + 1, int(0)))));
+  dp[0][0][0][0] = 1;
+  rep(i, n) rep(x, n * n) rep(e, i + 1) rep(y, i + 1 - e) {
+    if (dp[i][x][e][y] == 0) continue;
+    string S;
+    array<int, 3> use = {i - e - y, e, y};
+    each(e, s) if (use[e] == 0) S += e;
+    else --use[e];
+    rep(I, 3) rep(j, sz(S)) if (S[j] == I) {
+      dp[i + 1][x + j][e + (I == 1)][y + (I == 2)] += dp[i][x][e][y];
+      break;
+    }
+  }
+  int ans = 0;
+  V<int> sum(3);
+  each(e, s)++ sum[e];
+  ints(k);
+  rep(i, min(k + 1, n * n)) ans += dp[n][i][sum[1]][sum[2]];
+  wt(ans);
 }
