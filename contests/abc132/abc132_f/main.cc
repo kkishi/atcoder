c817b5319456ba1c318da6733ceb9b789fe8ffb4
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-12-04 19:12:40 +0100

    rewrite abc132_f with Quotients

--- contests/abc132/abc132_f/main.cc
+++ contests/abc132/abc132_f/main.cc
@@ -2,41 +2,27 @@
 
 #include "atcoder.h"
 #include "modint.h"
+#include "quotients.h"
 
 using mint = ModInt<>;
 
-struct S {
-  int x, c;
-};
-
 void Main() {
   ints(n, k);
-
-  set<int> x;
-  for (int i = 1; i * i <= n; ++i) {
-    x.insert(i);
-    x.insert(n / i);
-  }
-  map<int, int> c;
-  for (auto it = x.begin(); it != x.end(); ++it) {
-    auto jt = next(it);
-    if (jt == x.end()) {
-      assert(*it == n);
-      c[*it] = 1;
-    } else {
-      c[*it] = n / *it - n / *jt;
+  V<pair<int, int>> qs = Quotients(n);
+  sort(all(qs), greater{});
+  int N = sz(qs);
+  vector dp(k + 1, vector(N, mint(0)));
+  dp[0][0] = 1;
+  rep(i, k) {
+    V<mint> d(N + 1);
+    rep(j, N) {
+      d[0] += dp[i][j];
+      d[N - j] -= dp[i][j];
     }
-  }
-  vector<S> v;
-  for (auto [x, ci] : c) {
-    v.pb({x, ci});
-  }
-
-  VV<mint> dp(k + 1, V<mint>(v.size()));
-  rep(i, v.size()) dp[1][i] = v[i].c;
-  for (int i = 2; i <= k; ++i) {
-    rrep(j, v.size() - 1) dp[i - 1][j] += dp[i - 1][j + 1];
-    rep(j, v.size()) dp[i][j] = dp[i - 1][v.size() - 1 - j] * v[j].c;
+    rep(j, N) {
+      dp[i + 1][j] = d[j] * qs[j].second;
+      d[j + 1] += d[j];
+    }
   }
   wt(accumulate(all(dp[k]), mint(0)));
 }
