fcd17690f1e5b4ac68091528bb87fbb0b117452c
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-12-31 19:23:23 +0100

    add solutions

--- contests/past202112-open/past202112_o/main.cc
+++ contests/past202112-open/past202112_o/main.cc
@@ -1,29 +1,29 @@
 #include <bits/stdc++.h>
 
 #include <atcoder/convolution>
+#include <atcoder/modint>
 
 #include "atcoder.h"
-#include "modint.h"
 
-using mint = ModInt<998244353>;
+using mint = atcoder::modint998244353;
 
 void Main() {
   ints(n);
-  using ll = long long;
   const int N = 100000;
-  V<ll> a(N);
+  V<mint> a(N);
   rep(n) {
     ints(b);
     ++a[b - 1];
   }
-  V<ll> c(a.rbegin(), a.rend());
-  V<ll> b = atcoder::convolution(a, c);
+  V<mint> c(a.rbegin(), a.rend());
+  V<mint> b = atcoder::convolution(a, c);
   V<int> s(n);
   cin >> s;
   mint ans = 0;
   rep(k, 1, n) {
-    int v = b[N - 1 - k];
+    mint v = b[N - 1 - k];
     ans += (n - k) * v * s[k - 1];
   }
-  wt(ans * mint::Fact(n - 2));
+  rep(i, n - 2) ans *= i + 1;
+  wt(ans.val());
 }
