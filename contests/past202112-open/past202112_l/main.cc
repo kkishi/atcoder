f0605e41b9d399a538d84937a7b87e8b33c6ce59
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-01-07 14:56:25 +0100

    add solutions

--- contests/past202112-open/past202112_l/main.cc
+++ contests/past202112-open/past202112_l/main.cc
@@ -2,4 +2,20 @@
 
 #include "atcoder.h"
 
-void Main() {}
+void Main() {
+  ints(n, p);
+  V<int> a(n);
+  cin >> a;
+  V<int> lds;
+  rep(i, n) {
+    if (a[i] + i > p || a[i] - (n - 1 - i) < 0) continue;
+    int b = a[i] + i;
+    auto it = lower_bound(all(lds), b - 1, greater{});
+    if (it == lds.end()) {
+      lds.pb(b);
+    } else {
+      *it = b;
+    }
+  }
+  wt(n - sz(lds));
+}
