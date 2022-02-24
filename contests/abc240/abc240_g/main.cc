d1c3bcf6cd0c5f0ba89a9cd7e402cc1e8d0bb559
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-02-24 10:24:48 +0100

    add solutions

--- contests/abc240/abc240_g/main.cc
+++ contests/abc240/abc240_g/main.cc
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
+#include "modint.h"
+
+using mint = ModInt<998244353>;
 
 void Main() {
+  ints(n, x, y, z);
+  mint ans = 0;
+  rep(i, n + 1) {
+    int a = i + abs(z);
+    if (!even(a)) continue;
+    mint Z = mint::Comb(i, a / 2);
+    int j = n - i;
+    int x2 = abs(x - y);
+    int b = j + x2;
+    if (!even(b)) continue;
+    int y2 = abs(x + y);
+    int c = j + y2;
+    if (!even(c)) continue;
+    mint XY = mint::Comb(j, b / 2) * mint::Comb(j, c / 2);
+    ans += Z * XY * mint::Comb(n, i);
+  }
+  wt(ans);
 }
