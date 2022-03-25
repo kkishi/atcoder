ceffe140d181f2cf0bcc6d4ebadf1a4a9e4c9511
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-03-25 02:26:11 +0100

    Use MinCostFlow

--- contests/abc231/abc231_h/main.cc
+++ contests/abc231/abc231_h/main.cc
@@ -1,27 +1,19 @@
 #include <bits/stdc++.h>
-
-#include <atcoder/mincostflow>
+#include <min_cost_flow.h>
 
 #include "atcoder.h"
 
 void Main() {
   ints(h, w, n);
-  atcoder::mcf_graph<int, int> g(h + w + 4);
-  int s = h + w, t = s + 1, S = t + 1, T = S + 1;
+  int s = h + w, t = s + 1;
 
-  rep(i, h) g.add_edge(s, i, big, 0);
-  rep(i, w) g.add_edge(h + i, t, big, 0);
+  MinCostFlow g;
+  rep(i, h) g.Add(s, i, big, 0, 1);
+  rep(i, w) g.Add(h + i, t, big, 0, 1);
   rep(n) {
     ints(a, b, c);
     --a, --b;
-    g.add_edge(a, h + b, 1, c);
+    g.Add(a, h + b, 1, c);
   }
-  g.add_edge(t, s, big, 0);
-
-  rep(i, h) g.add_edge(S, i, 1, 0);
-  rep(i, w) g.add_edge(h + i, T, 1, 0);
-  g.add_edge(s, T, h, 0);
-  g.add_edge(S, t, w, 0);
-
-  wt(g.flow(S, T).second);
+  wt(g.Flow(s, t).second);
 }
--- contests/past202005-open/past202005_o/main.cc
+++ contests/past202005-open/past202005_o/main.cc
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
-
-#include <atcoder/mincostflow>
+#include <min_cost_flow.h>
 
 #include "atcoder.h"
 #include "pow.h"
@@ -13,32 +12,20 @@ void Main() {
 #define round(i) (i)
 #define pin_round(i, j) (round(3) + ((i)*3 + (j)))
 #define pin(i) (pin_round(n, 0) + (i))
-  int s = pin(n), t = s + 1, S = t + 1, T = S + 1;
+  int s = pin(n), t = s + 1;
 
-  atcoder::mcf_graph<int, int> g(T + 1);
-#define e g.add_edge
-  // rep(i, 3) e(s, round(i), m, 0);
-  int sum = 0;
+  MinCostFlow g;
+  rep(i, 3) g.Add(s, round(i), m, 0, m);
   rep(i, 3) rep(j, n) {
     int x = a[j] * Pow(b[j], i + 1) % r[i];
-    sum -= x;
-    e(pin_round(j, i), round(i), 1, x);
+    g.Add(round(i), pin_round(j, i), 1, -x);
   }
-  rep(i, 3) rep(j, n) e(pin_round(j, i), pin(j), 1, 0);
+  rep(i, 3) rep(j, n) g.Add(pin_round(j, i), pin(j), 1, 0);
   auto cost = [](int i, int a, int b) {
     if (i == 1) return a * b;
     if (i == 2) return a * b * (b - 1);
     return a * b * b * (b - 1);
   };
-  rep(i, 3) rep(j, n) e(pin(j), t, 1, cost(i + 1, a[j], b[j]));
-
-  e(t, s, big, 0);
-
-  rep(i, 3) rep(j, n) e(S, pin_round(j, i), 1, 0);
-  rep(i, 3) e(round(i), T, n, 0);
-
-  rep(i, 3) e(S, round(i), m, 0);
-  e(s, T, m * 3, 0);
-
-  wt(-(sum + g.flow(S, T).second));
+  rep(i, 3) rep(j, n) g.Add(pin(j), t, 1, cost(i + 1, a[j], b[j]));
+  wt(-g.Flow(s, t).second);
 }
