420bdb3dca86da1e9761eb322e56e2da8f9d1eef
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-04-11 14:56:49 +0200

    add solutions

--- contests/agc003/agc003_c/main.cc
+++ contests/agc003/agc003_c/main.cc
@@ -3,4 +3,17 @@
 #include "atcoder.h"
 
 void Main() {
+  ints(n);
+  V<int> a(n);
+  cin >> a;
+  V<int> b = a;
+  sort(b);
+  map<int, int> m;
+  for (int i = 0; i < n; i += 2) {
+    m[b[i]]++;
+    m[a[i]]--;
+  }
+  int ans = 0;
+  each(_, x, m) if (x > 0) ans += x;
+  wt(ans);
 }
