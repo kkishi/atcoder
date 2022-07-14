70efd941b2ed71be8b3583c0f0712e072e3c7eed
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-07-14 12:39:08 +0200

    add solutions

--- contests/abc259/abc259_h/naive.cc
+++ contests/abc259/abc259_h/naive.cc
@@ -10,10 +10,7 @@ void Main() {
   vector a(n, vector(n, int(0)));
   cin >> a;
   mint ans = 0;
-  map<int, int> mp;
-  each(e, a) each(f, e)++ mp[--f];
-  V<bool> sm(n * n);
-  each(k, v, mp) if (v >= n) {
+  rep(k, 1, n * n + 1) {
     vector dp(n, vector(n, mint(0)));
     rep(i, n) rep(j, n) {
       if (a[i][j] == k) {
@@ -22,18 +19,6 @@ void Main() {
       }
       if (i + 1 < n) dp[i + 1][j] += dp[i][j];
       if (j + 1 < n) dp[i][j + 1] += dp[i][j];
-    }
-  }
-  else {
-    sm[k] = true;
-  }
-  VV<pair<int, int>> v(n * n);
-  rep(i, n) rep(j, n) if (sm[a[i][j]]) v[a[i][j]].eb(i, j);
-  each(e, v) {
-    rep(j, sz(e)) rep(i, j + 1) {
-      int x = e[j].first - e[i].first;
-      int y = e[j].second - e[i].second;
-      if (y >= 0) ans += mint::Comb(x + y, x);
     }
   }
   wt(ans);
