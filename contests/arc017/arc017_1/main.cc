92538720b3d76e8530581c8107771844a0a44fd1
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-07-15 11:03:16 +0200

    add solutions

--- contests/arc017/arc017_1/main.cc
+++ contests/arc017/arc017_1/main.cc
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
+#include "divisors.h"
 
 void Main() {
+  ints(n);
+  wt(sz(Divisors(n)) == 2 ? "YES" : "NO");
 }
--- contests/arc017/arc017_2/main.cc
+++ contests/arc017/arc017_2/main.cc
@@ -3,4 +3,14 @@
 #include "atcoder.h"
 
 void Main() {
+  ints(n, k);
+  V<int> a(n);
+  cin >> a;
+  V<int> x;
+  x.pb(0);
+  rep(i, n - 1) if (a[i] >= a[i + 1]) x.pb(i + 1);
+  x.pb(n);
+  int ans = 0;
+  rep(i, sz(x) - 1) ans += max(0, x[i + 1] - x[i] - k + 1);
+  wt(ans);
 }
