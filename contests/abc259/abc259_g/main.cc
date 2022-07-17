613f016f026a89ddcc47ff0ecccef3b82dda46ef
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-07-17 23:21:30 +0200

    add solutions

--- contests/abc259/abc259_g/main.cc
+++ contests/abc259/abc259_g/main.cc
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 
+#include <atcoder/maxflow>
+
 #include "atcoder.h"
 
 void Main() {
+  ints(h, w);
+  vector a(h, vector(w, int(0)));
+  cin >> a;
+  atcoder::mf_graph<int> g(h + w + 2);
+  int s = h + w, t = s + 1;
+  int sum = 0;
+  rep(i, h) rep(j, w) {
+    int x = a[i][j];
+    if (x > 0) sum += x * 2;
+    int r = i;
+    int c = h + j;
+    if (x >= 0) {
+      g.add_edge(s, r, x);
+      g.add_edge(r, c, x);
+      g.add_edge(c, t, x);
+    } else {
+      g.add_edge(s, c, -x);
+      g.add_edge(r, c, big);
+      g.add_edge(r, t, -x);
+    }
+  }
+  wt(sum - g.flow(s, t));
 }
