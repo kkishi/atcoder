3022e8e9baf2c700d9b717c8cac3fc5508fb2640
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-09-24 16:05:27 +0200

    simplify

--- contests/abc270/abc270_d/main.cc
+++ contests/abc270/abc270_d/main.cc
@@ -6,19 +6,7 @@ void Main() {
   ints(n, k);
   V<int> a(k);
   cin >> a;
-  V<pair<int, int>> dp(n + 1);
-  rep(i, n + 1) {
-    int X = 0;
-    int Y = i;
-    each(e, a) if (e <= i) {
-      auto [x, y] = dp[i - e];
-      y += e;
-      if (y > X) {
-        X = y;
-        Y = x;
-      }
-    }
-    dp[i] = {X, Y};
-  }
-  wt(dp[n].first);
+  V<int> dp(n + 1);
+  rep(i, n + 1) each(e, a) if (e <= i) chmax(dp[i], i - dp[i - e]);
+  wt(dp[n]);
 }
