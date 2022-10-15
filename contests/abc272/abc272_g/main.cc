319065c2eb25408a8dc2f6f489494819290b5666
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-10-15 13:36:04 +0200

    add solutions

--- contests/abc272/abc272_g/main.cc
+++ contests/abc272/abc272_g/main.cc
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
+#include "divisors.h"
+#include "rand.h"
 
 void Main() {
+  ints(n);
+  V<int> a(n);
+  cin >> a;
+  Rand r;
+  int ans = -1;
+  rep(100) {
+    int i = r.Int(0, n - 1);
+    int j = r.Int(0, n - 1);
+    if (i == j) continue;
+    each(d, Divisors(abs(a[i] - a[j]))) {
+      if (3 <= d) {
+        int cnt = 0;
+        rep(k, n) if (a[k] % d == a[i] % d)++ cnt;
+        if (cnt * 2 > n) ans = d;
+      }
+    }
+  }
+  wt(ans);
 }
