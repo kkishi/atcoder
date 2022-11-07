0385e8f328780f07a203983b4fb39db49422ad20
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-11-07 08:49:48 +0100

    simplify

--- contests/abc276/abc276_g/main.cc
+++ contests/abc276/abc276_g/main.cc
@@ -12,23 +12,17 @@ void Main() {
     return;
   }
   // let f(x) = C(n-1, x)
-  // sum[i] = f(0) + f(1) + ... + f(i)
+  // step 1: sum[i] = f(i)
+  // step 2: sum[i] = f(0) + f(1) + ... + f(i)
+  // step 3: sum[i] = (i+1)*f(0) + i*f(1) + ... + f(i)
   V<mint> sum(m + 1);
-  rep(i, sz(sum)) {
-    if (i > 0) sum[i] = sum[i - 1];
-    sum[i] += mint::Comb(n - 1, i);
-  }
-  // sum2[i] = (i+1)*f(0) + i*f(1) + ... + f(i)
-  V<mint> sum2(m + 1);
-  rep(i, sz(sum2)) {
-    if (i > 0) sum2[i] = sum2[i - 1];
-    sum2[i] += sum[i];
-  }
+  rep(i, m + 1) sum[i] = mint::Comb(n - 1, i);
+  rep(2) rep(i, 1, m + 1) sum[i] += sum[i - 1];
   int rem = m - (n - 1);
   mint ans = 0;
   rep(x, (rem / 3) + 1) {
     mint a = mint::Comb(x + n - 2, n - 2);  // H(n - 1, x)
-    mint b = sum2[rem - x * 3];
+    mint b = sum[rem - x * 3];
     ans += a * b;
   }
   wt(ans);
