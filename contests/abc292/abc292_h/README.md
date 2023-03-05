bfd8863ab3fd8a32d82293ef646fa8ac27e931b0
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-03-05 16:51:01 +0100

    add solutions

---
+++ contests/abc292/abc292_h/README.md
@@ -0,0 +1,4 @@
+# 典型要素
+
+[平均は分母を払って言い換え](https://twitter.com/_primenumber/status/1632013100978421763)
+[「Aiの平均がx以上か？」は「Ai-xの合計が0以上か？」に読み替える典型](https://twitter.com/kyopro_friends/status/1632019523732905984)
--- contests/abc292/abc292_h/main.cc
+++ contests/abc292/abc292_h/main.cc
@@ -1,6 +1,31 @@
 #include <bits/stdc++.h>
+#include <lazysegtree.h>
 
 #include "atcoder.h"
+#include "bit.h"
+
+bool g(add_min::S x) { return x > 0; }
 
 void Main() {
+  ints(n, b, q);
+  V<int> a(n);
+  cin >> a;
+  V<int> s = a;
+  rep(i, 1, n) s[i] += s[i - 1];
+  V<int> v(n);
+  rep(i, n) v[i] = b * (i + 1) - s[i];
+  add_min::segtree t(v);
+  BIT<int> bit(n);
+  rep(i, n) bit.Set(i, a[i]);
+  rep(q) {
+    ints(c, x);
+    --c;
+    t.apply(c, n, a[c]);
+    a[c] = x;
+    bit.Set(c, x);
+    t.apply(c, n, -a[c]);
+    int r = t.max_right<g>(0);
+    chmin(r, n - 1);
+    wt((double)bit.Sum(r) / (r + 1));
+  }
 }
