fe7df834f42eafa000483fdeae40b9c32b36b348
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-03-06 11:30:45 +0100

    add comments

--- contests/abc292/abc292_g/main.cc
+++ contests/abc292/abc292_g/main.cc
@@ -11,22 +11,38 @@ void Main() {
   cin >> s;
   vector memo(n + 1, vector(n + 1, vector(m + 1, vector(10, mint(0)))));
   vector seen(n + 1, vector(n + 1, vector(m + 1, vector(10, false))));
-  wt(Fix([&](auto rec, int l, int r, int depth, int next_digit) -> mint {
+  wt(Fix([&](auto rec, int l, int r, int digit, int next_num) -> mint {
+    // If the interval size is 0, we return the identity for multiplication.
     if (l == r) return 1;
-    if (depth == m) return l + 1 == r;
-    if (next_digit == 10) return 0;
-    if (seen[l][r][depth][next_digit]) return memo[l][r][depth][next_digit];
-    seen[l][r][depth][next_digit] = true;
-    mint ret = 0;
-    rep(mid, l + 1, r + 1) rep(digit, next_digit, 10) {
-      bool ok = true;
-      rep(i, l, mid) {
-        char c = s[i][depth];
-        if (c != '?' && (c - '0') != digit) ok = false;
+    // If there's no more digits left, the interval size has to be 1, otherwise
+    // it violates the requirement that elemends need to be strictly increasing.
+    if (digit == m) return l + 1 == r;
+    // If there's no more numbers left, and the interval size is not 0, there's
+    // no valid assignment.
+    if (next_num == 10) return 0;
+    mint& ret = memo[l][r][digit][next_num];
+    if (!seen[l][r][digit][next_num]) {
+      seen[l][r][digit][next_num] = true;
+      rep(num, next_num, 10) {
+        rep(mid, l + 1, r + 1) {
+          char c = s[mid - 1][digit];
+          // We can no longer advance the mid point using num.
+          if (c != '?' && (c - '0') != num) break;
+          // We split the interval into two parts where we can count
+          // independently:
+          //
+          // 1) [l, mid) where we chose to use num for digit. The elements in
+          // this interval are still all equal, so they need to be ordered using
+          // the remaining digits.
+          // 2) [mid, r) where we continue to choose how
+          // to break up using the remaining numbers.
+          //
+          // Note that now the elements in 1) are ensured to be smaler than the
+          // elements in 2).
+          ret += rec(l, mid, digit + 1, 0) * rec(mid, r, digit, num + 1);
+        }
       }
-      if (ok) ret += rec(l, mid, depth + 1, 0) * rec(mid, r, depth, digit + 1);
     }
-    memo[l][r][depth][next_digit] = ret;
     return ret;
   })(0, n, 0, 0));
 }
