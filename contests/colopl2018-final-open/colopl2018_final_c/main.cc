43de0e9bb02269febae6f4cd098fb5364ddf4fc9
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-03-08 23:23:00 +0100

    add solutions

---
+++ contests/colopl2018-final-open/colopl2018_final_c/main.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include "atcoder.h"
+#include "convex_hull_trick.h"
+
+void Main() {
+  ints(n);
+  V<int> a(n);
+  cin >> a;
+  // min(a[j] + (j - i)^2)
+  // = min(a[j] + j^2 - 2ij) + i^2
+  MinConvexHullTrick cht;
+  rep(i, n) cht.Add(-2 * i, a[i] + i * i);
+  rep(i, n) wt(cht.Get(i) + i * i);
+}
