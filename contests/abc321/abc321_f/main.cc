71318efaf3593ce16a14baf5b76bd1d9b60e3d60
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-09-24 16:01:50 +0200

    add solutions

--- contests/abc321/abc321_f/main.cc
+++ contests/abc321/abc321_f/main.cc
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 
 #include "atcoder.h"
+#include "modint.h"
+
+using mint = ModInt<998244353>;
 
 void Main() {
+  ints(q, k);
+  vector dp(5001, mint(0));
+  dp[0] = 1;
+  rep(q) {
+    rd(char, op);
+    ints(x);
+    if (op == '+') {
+      rrep(i, x, 5001) dp[i] += dp[i - x];
+    } else {
+      rep(i, x, 5001) dp[i] -= dp[i - x];
+    }
+    wt(dp[k]);
+  }
 }
